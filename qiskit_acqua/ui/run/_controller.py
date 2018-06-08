0a2f8960ae5e30582e124f4e1b88dc594d76bb5f
Manoel Marques <manoel@us.ibm.com>
2018-06-08 07:17:16 -0400

    UI windows use python.exe process

--- qiskit_acqua/ui/run/_controller.py
+++ qiskit_acqua/ui/run/_controller.py
@@ -585,15 +585,27 @@ class AlgoritthmThread(threading.Thread):
             else:
                 if sys.platform == 'win32' and process_name.endswith('pythonw.exe'):
                     path = os.path.dirname(process_name)
-                    files = [f for f in os.listdir(path) if f != 'pythonw.exe' and f.startswith('python') and f.endswith('.exe')]   
+                    files = [f for f in os.listdir(path) if f != 'pythonw.exe' and f.startswith('python') and f.endswith('.exe')]
+                    # sort reverse to have hihre python versions first: python3.exe before python2.exe
+                    files = sorted(files,key=str.lower, reverse=True)
+                    new_process = None
                     for file in files:
                         p = os.path.join(path,file)
                         if os.path.isfile(p):
-                            startupinfo = subprocess.STARTUPINFO()
-                            startupinfo.dwFlags = subprocess.STARTF_USESHOWWINDOW
-                            startupinfo.wShowWindow = subprocess.SW_HIDE
-                            process_name = p
-                            break
+                            # python.exe takes precedence
+                            if file.lower() == 'python.exe':
+                                new_process = p
+                                break
+                            
+                            # use first found
+                            if new_process is None:
+                                new_process = p
+                        
+                    if new_process is not None:
+                        startupinfo = subprocess.STARTUPINFO()
+                        startupinfo.dwFlags = subprocess.STARTF_USESHOWWINDOW
+                        startupinfo.wShowWindow = subprocess.SW_HIDE
+                        process_name = new_process
                 
             if self._output is not None and logger.getEffectiveLevel() == logging.DEBUG:
                 self._output.write('Process: {}\n'.format(process_name))
