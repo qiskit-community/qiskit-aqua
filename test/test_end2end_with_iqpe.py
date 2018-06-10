1d5bb5e5428a0dc8426332aeb84b101248ad4143
Shaohan Hu <shaohan.hu@ibm.com>
2018-06-10 16:25:03 -0400

    update iqpe e2e test to be similar to that of qpe

--- test/test_end2end_with_iqpe.py
+++ test/test_end2end_with_iqpe.py
@@ -16,8 +16,10 @@
 # =============================================================================
 
 import unittest
+from parameterized import parameterized
 from collections import OrderedDict
-
+import numpy as np
+from qiskit_acqua.utils import decimal_to_binary
 from qiskit_acqua import get_algorithm_instance, get_initial_state_instance
 from test.common import QISKitAcquaChemistryTestCase
 from qiskit_acqua_chemistry.drivers import ConfigurationManager
@@ -27,12 +29,17 @@ from qiskit_acqua_chemistry import FermionicOperator
 class TestIQPE(QISKitAcquaChemistryTestCase):
     """IQPE tests."""
 
-    def setUp(self):
+    @parameterized.expand([
+        [0.5],
+        [0.735],
+        [1],
+    ])
+    def test_iqpe(self, distance):
         self.algorithm = 'IQPE'
-        self.log.debug('Testing IQPE with H2')
+        self.log.debug('Testing End-to-End with IQPE on H2 with interatomic distance {}.'.format(distance))
         cfg_mgr = ConfigurationManager()
         pyscf_cfg = OrderedDict([
-            ('atom', 'H .0 .0 .0; H .0 .0 0.735'),
+            ('atom', 'H .0 .0 .0; H .0 .0 {}'.format(distance)),
             ('unit', 'Angstrom'),
             ('charge', 0),
             ('spin', 0),
@@ -44,7 +51,7 @@ class TestIQPE(QISKitAcquaChemistryTestCase):
         self.molecule = driver.run(section)
 
         ferOp = FermionicOperator(h1=self.molecule._one_body_integrals, h2=self.molecule._two_body_integrals)
-        self.qubitOp = ferOp.mapping(map_type='JORDAN_WIGNER', threshold=1e-10)
+        self.qubitOp = ferOp.mapping(map_type='PARITY', threshold=1e-10).two_qubit_reduced_operator(2)
 
         exact_eigensolver = get_algorithm_instance('ExactEigensolver')
         exact_eigensolver.init_args(self.qubitOp, k=1)
@@ -52,29 +59,47 @@ class TestIQPE(QISKitAcquaChemistryTestCase):
         self.reference_energy = results['energy']
         self.log.debug('The exact ground state energy is: {}'.format(results['energy']))
 
-    def test_qpe(self):
-        num_particles = self.molecule._num_alpha + self.molecule._num_beta
-        two_qubit_reduction = False
-        num_orbitals = self.qubitOp.num_qubits + (2 if two_qubit_reduction else 0)
-        qubit_mapping = 'jordan_wigner'
 
-        num_time_slices = 1
-        num_iterations = 5
+        num_particles = self.molecule._num_alpha + self.molecule._num_beta
+        two_qubit_reduction = True
+        num_orbitals = self.qubitOp.num_qubits + (2 if two_qubit_reduction else 0)
+        qubit_mapping = 'parity'
+
+
+        num_time_slices = 100
+        num_iterations = 12
 
         iqpe = get_algorithm_instance('IQPE')
-        iqpe.setup_quantum_backend(backend='local_qasm_simulator', shots=20)
+        iqpe.setup_quantum_backend(backend='local_qasm_simulator', shots=1, skip_translation=False)
 
         state_in = get_initial_state_instance('HartreeFock')
         state_in.init_args(self.qubitOp.num_qubits, num_orbitals, qubit_mapping, two_qubit_reduction, num_particles)
 
-        iqpe.init_args(self.qubitOp, state_in, num_time_slices, num_iterations,
-                       paulis_grouping='default', expansion_mode='trotter', expansion_order=2)
+        iqpe.init_args(
+            self.qubitOp, state_in, num_time_slices, num_iterations,
+            paulis_grouping='random',
+            expansion_mode='suzuki',
+            expansion_order=2,
+        )
 
         result = iqpe.run()
 
-        self.log.debug('phase estimation:  {}'.format(result['phase']))
-        self.log.debug('energy estimation: {}'.format(result['energy']))
-        self.log.debug('reference energy:  {}'.format(self.reference_energy))
+        self.log.debug('top result str label:     {}'.format(result['top_measurement_label']))
+        self.log.debug('top result in decimal:    {}'.format(result['top_measurement_decimal']))
+        self.log.debug('stretch:                  {}'.format(result['stretch']))
+        self.log.debug('translation:              {}'.format(result['translation']))
+        self.log.debug('final energy from QPE:    {}'.format(result['energy']))
+        self.log.debug('reference energy:         {}'.format(self.reference_energy))
+        self.log.debug('ref energy (transformed): {}'.format(
+            (self.reference_energy + result['translation']) * result['stretch'])
+        )
+        self.log.debug('ref binary str label:     {}'.format(decimal_to_binary(
+            (self.reference_energy + result['translation']) * result['stretch'],
+            max_num_digits=num_iterations + 3,
+            fractional_part_only=True
+        )))
+
+        np.testing.assert_approx_equal(result['energy'], self.reference_energy, significant=2)
 
 
 if __name__ == '__main__':
