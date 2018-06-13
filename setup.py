580573d88b65a78494f085f1ed576cf1cbb2e481
Manoel Marques <manoel@us.ibm.com>
2018-06-13 15:28:21 -0400

    version 0.1.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requirements = [
 
 setuptools.setup(
     name='qiskit-acqua',
-    version="0.1.0",  # this should match __init__.__version__
+    version="0.1.1",  # this should match __init__.__version__
     description='QISKit ACQUA: An extensible library of quantum computing algorithms',
     long_description=long_description,
     long_description_content_type="text/markdown",
