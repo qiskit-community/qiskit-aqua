a194557ba754f9b14d473ff9e39a2bc2449e58c1
Richard Chen <chenrich@us.ibm.com>
2018-07-06 11:49:40 -0400

    revise the way to derive most like results if vector is a dict.

--- qiskit_acqua/ising/stableset.py
+++ qiskit_acqua/ising/stableset.py
@@ -138,9 +138,9 @@ def stableset_value(x, w):
         float, bool: size of the stable set, and Boolean indicating
             feasibility.
     """
-    assert(len(x) == len(w))
+    assert(len(x) == w.shape[0])
     feasible = True
-    num_nodes = len(w)
+    num_nodes = w.shape[0]
     for i in range(num_nodes):
         for j in range(i+1, num_nodes):
             if w[i, j] != 0 and x[i] == 0 and x[j] == 0:
@@ -159,31 +159,27 @@ def get_graph_solution(x):
     """
     return 1 - x
 
-def sample_most_likely(n, state_vector):
+def sample_most_likely(state_vector):
     """Compute the most likely binary string from state vector.
 
     Args:
-        n (int): number of  qubits.
         state_vector (numpy.ndarray or dict): state vector or counts.
 
     Returns:
         numpy.ndarray: binary string as numpy.ndarray of ints.
     """
     if isinstance(state_vector, dict) or isinstance(state_vector, OrderedDict):
-        temp_vec = np.zeros(2**n)
-        total = 0
-        for i in range(2**n):
-            state = np.binary_repr(i, n)
-            count = state_vector.get(state, 0)
-            temp_vec[i] = count
-            total += count
-        state_vector = temp_vec / float(total)
-
-    k = np.argmax(np.abs(state_vector))
-    x = np.zeros(n)
-    for i in range(n):
-        x[i] = k % 2
-        k >>= 1
-    return x
+        # get the binary string with the largest count
+        binary_string = sorted(state_vector.items(), key=lambda kv: kv[1])[-1][0]
+        x = np.asarray([int(y) for y in list(binary_string)])
+        return x
+    else:
+        n = int(np.log2(state_vector.shape[0]))
+        k = np.argmax(np.abs(state_vector))
+        x = np.zeros(n)
+        for i in range(n):
+            x[i] = k % 2
+            k >>= 1
+        return x
 
 
