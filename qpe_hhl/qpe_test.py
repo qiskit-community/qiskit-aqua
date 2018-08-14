3f09b38a0ad489c4ede1fe4358fd66aaa2e8ff9e
Isabel Haide <isabel.haide@ibm.com>
2018-08-14 10:33:08 +0200

    Included changed qpe, sparse matrix generator in qpe_test

--- qpe_hhl/qpe_test.py
+++ qpe_hhl/qpe_test.py
@@ -1,30 +1,31 @@
 from qpe import QPE
 from qiskit_aqua import Operator
-
+import scipy
 import numpy as np
 
 import matplotlib.pyplot as plt
 
 qpe = QPE()
-
-w = [-1, -1]
 n = 4
-while min(w) <= 0:
-    matrix = np.random.random([n, n])+1j*np.random.random([n, n])
-    matrix = 4*(matrix+matrix.T.conj())
+v = np.array([-1, -1, -1, -1])
+print(v)
 
+while np.amin(v) <= 0:
+    matrix = scipy.sparse.random(n, n, density = 0.4, data_rvs = np.random.randn)
+    matrix = np.round(5 *( matrix.A + np.matrix.getH(matrix.A)), 3)
+    v = np.linalg.eigvals(matrix)
     op = Operator(matrix=matrix)
-    w, v = np.linalg.eig(matrix)
+    
 
-matrix = np.array([[3, 0, 0 , 0], [0, 2.7, 0, 0], [0, 0, 11.8, 0], [0, 0, 0, 5.3]])
+#matrix = np.array([[3, 0, 0 , 0], [0, 2.7, 0, 0], [0, 0, 11.8, 0], [0, 0, 0, 5.3]])
 op = Operator(matrix=matrix)
 
 print(matrix)
-print(max(abs(w))/min(abs(w)))
-print(w.real)
+print(np.amax(abs(v))/np.amin(abs(v)))
+print(v.real)
 print(v)
 
-invec = sum([v[:,i] for i in range(n)])
+invec = v
 invec /= np.sqrt(invec.dot(invec.conj()))
 #op = Operator(matrix=1/4*np.array([[15, 9, 5, -3], [9, 15, 3, -5], [5, 3, 15, -9], [-3, -5, -9, 15]]))
 #invec = [0,0,0,1]
@@ -33,9 +34,9 @@ params = {
 'algorithm': {
         'name': 'QPE',
         'num_ancillae': 4,
-        'num_time_slices': 50,
+        'num_time_slices': 10,
         'expansion_mode': 'suzuki',
-        'expansion_order': 4,
+        'expansion_order': 3,
         #'evo_time': np.pi/2
 },
 "iqft": {
