2a1a807f6beee56f10eeb670f6d6df7973997ce8
David Bucher <d.bucher97@gmail.com>
2018-09-11 13:12:16 +0200

    fixed point

--- newton/newton.py
+++ newton/newton.py
@@ -10,20 +10,29 @@ def float_newton_iteration(x0, lam, steps=1):
 def get_x0(lam):
     return 2**-np.ceil(np.log2(lam))
 
-def fp_newton(lam, prec=8, steps=10):
+def fp_newton(lam, prec=8, steps=2):
     x = FPDecimal(n=prec, val=get_x0(lam))
     l = FPInteger(n=prec, val=lam)
     t = FPInteger(n=prec, val=2)
     for i in range(steps):
-        x = x*t-x*x*l
+        x = x*t-(x*x)*l
     return x.to_float()
 
 
 prec = 32
-lams = np.arange(2**8-1)+1
+lams = np.arange(2**16-1)+1
 res = []
 for l in lams:
     res.append(fp_newton(l, prec=prec))
 plt.plot(lams, res)
 plt.plot(lams, 1/lams)
 plt.show()
+
+# prec = 8
+#
+# v = 4
+# x = FPInteger(n=prec, val=v)
+# y = FPDecimal(n=prec, val=1/v)
+# print(x, y)
+# print(y*x)
+# print(x*y)
