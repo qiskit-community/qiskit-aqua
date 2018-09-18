c7b90ce622d1866401ad422d38fd00ce8598d8cb
David Bucher <d.bucher97@gmail.com>
2018-09-18 09:26:30 +0200

    cleanup

--- demo/svm/install.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-INSTALL_DIR=$HOME/.demo/svm/
-
-if ! [ -d $INSTALL_DIR ]; then
-  mkdir $INSTALL_DIR -p
-  tar -xf img/data.tar.gz -C $INSTALL_DIR
-  echo "Installed OCR image files to $INSTALL_DIR"
-fi
