c4149e5a85a710899bbfb9612bfa36f4825af1a4
Richard Chen <chenrich@us.ibm.com>
2018-12-03 15:34:35 -0500

    fix typo when merging the stable branch

--- qiskit_aqua/utils/run_circuits.py
+++ qiskit_aqua/utils/run_circuits.py
@@ -147,7 +147,7 @@ def run_circuits(circuits, backend, execute_config, qjob_config=None,
     with_autorecover = False if backend.configuration().simulator else True
     max_circuits_per_job = sys.maxsize if backend.configuration().local else MAX_CIRCUITS_PER_JOB
 
-    support_qobj = my_backend.configuration().get('allow_q_object', False)
+    support_qobj = backend.configuration().get('allow_q_object', False)
     job_completed_signature = 'COMPLETED' if not support_qobj else 'Successful completion'
 
     qobjs = []
