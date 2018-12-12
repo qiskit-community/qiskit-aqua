d42b85e5be72c7648cf9a2f8222855badfcb921b
Stefan Woerner <wor@zurich.ibm.com>
2018-12-12 20:34:12 +0100

    bug fix in european call delta + minor renaming

--- qiskit_aqua/algorithms/components/uncertainty_problems/european_call_delta.py
+++ qiskit_aqua/algorithms/components/uncertainty_problems/european_call_delta.py
@@ -76,7 +76,7 @@ class EuropeanCallDelta(UncertaintyProblem):
         # map strike price to {0, ..., 2^n-1}
         lb = uncertainty_model.low
         ub = uncertainty_model.high
-        self._mapped_strike_price = int(np.round((strike_price - lb)/(ub - lb) * (uncertainty_model.num_values - 1)))
+        self._mapped_strike_price = int(np.ceil((strike_price - lb)/(ub - lb) * (uncertainty_model.num_values - 1)))
 
         # create comparator
         self._comparator = FixedValueComparator(uncertainty_model.num_target_qubits + 1, self._mapped_strike_price)
