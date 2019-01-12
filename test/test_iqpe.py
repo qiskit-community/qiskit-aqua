39df564c8091c72578641f2530b98b2fa6c485d2
Shaohan Hu <shaohan.hu@ibm.com>
2019-01-11 22:06:09 -0500

    update iqpe test

--- test/test_iqpe.py
+++ test/test_iqpe.py
@@ -32,6 +32,14 @@ from qiskit_aqua.algorithms import ExactEigensolver
 from qiskit_aqua.components.initial_states import Custom
 
 
+X = np.array([[0, 1], [1, 0]])
+Y = np.array([[0, -1j], [1j, 0]])
+Z = np.array([[1, 0], [0, -1]])
+I = np.array([[1, 0], [0, 1]])
+h1 = X + Y + Z + I
+qubitOp_simple = Operator(matrix=h1)
+
+
 pauli_dict = {
     'paulis': [
         {"coeff": {"imag": 0.0, "real": -1.052373245772859}, "label": "II"},
@@ -48,9 +56,10 @@ class TestIQPE(QiskitAquaTestCase):
     """IQPE tests."""
 
     @parameterized.expand([
-        [qubitOp_h2_with_2_qubit_reduction],
+        [qubitOp_simple, 'qasm_simulator'],
+        [qubitOp_h2_with_2_qubit_reduction, 'statevector_simulator'],
     ])
-    def test_iqpe(self, qubitOp):
+    def test_iqpe(self, qubitOp, simulator):
         self.algorithm = 'IQPE'
         self.log.debug('Testing IQPE')
 
@@ -83,7 +92,7 @@ class TestIQPE(QiskitAquaTestCase):
         iqpe = IQPE(self.qubitOp, state_in, num_time_slices, num_iterations,
                     paulis_grouping='random', expansion_mode='suzuki', expansion_order=2, shallow_circuit_concat=True)
 
-        backend = get_aer_backend('qasm_simulator')
+        backend = get_aer_backend(simulator)
         quantum_instance = QuantumInstance(backend, shots=100, pass_manager=PassManager())
 
         result = iqpe.run(quantum_instance)
