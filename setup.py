aeef3abcb7c55bc263668fd6905ab3a74a0085e4
Manoel Marques <manoel@us.ibm.com>
2019-01-21 12:15:17 -0500

    Add qiskit-ibmq-provider dependency

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ long_description="""<a href="https://qiskit.org/aqua" rel=nofollow>Qiskit Aqua</
 
 requirements = [
     "qiskit-terra>=0.8.0,<0.9",
+    "qiskit-ibmq-provider",
     "scipy>=0.19,!=0.19.1",
     "sympy>=1.3",
     "numpy>=1.13",
