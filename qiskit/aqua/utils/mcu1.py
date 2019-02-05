84336f92cf87012103cb8536d75bb71df9f978a7
Albert Frisch <alfr@de.ibm.com>
2019-02-05 13:20:59 +0100

    remove usages of CompositeGate for mcu1 and mcu3

--- qiskit/aqua/utils/mcu1.py
+++ qiskit/aqua/utils/mcu1.py
@@ -20,79 +20,62 @@ Multiple-Control U1 gate. Not using ancillary qubits.
 
 from sympy.combinatorics.graycode import GrayCode
 from qiskit import QuantumCircuit, QuantumRegister
-from qiskit.circuit import CompositeGate
 from qiskit.aqua.utils.controlledcircuit import apply_cu1
 from numpy import angle
 
 
-class MCU1Gate(CompositeGate):
-    """MCU1 gate."""
+def _apply_mcu1(circuit, theta, ctls, tgt, global_phase=0):
+    """Apply multi-controlled u1 gate from ctls to tgt with angle theta."""
 
-    def __init__(self, theta, ctls, tgt, circ=None):
-        """Create new MCU1 gate."""
-        self._ctl_bits = ctls
-        self._tgt_bits = tgt
-        self._theta = theta
-        qubits = [v for v in ctls] + [tgt]
-        n_c = len(ctls)
-        super(MCU1Gate, self).__init__("mcu1", (theta, n_c), qubits, circ)
+    n = len(ctls)
 
-        if n_c == 1:  # cx
-            apply_cu1(circ, theta, ctls[0], tgt)
-        else:
-            self.apply_mcu1(theta, ctls, tgt, circ)
+    gray_code = list(GrayCode(n).generate_gray())
+    last_pattern = None
 
-    def reapply(self, circ):
-        """Reapply this gate to corresponding qubits in circ."""
-        self._modifiers(circ.mcu1(self._theta, self._ctl_bits, self._tgt_bits))
+    theta_angle = theta*(1/(2**(n-1)))
+    gp_angle = angle(global_phase)*(1/(2**(n-1)))
 
-    def apply_mcu1(self, theta, ctls, tgt, circuit, global_phase=0):
-        """Apply multi-controlled u1 gate from ctls to tgt with angle theta."""
-
-        n = len(ctls)
-
-        gray_code = list(GrayCode(n).generate_gray())
-        last_pattern = None
-
-        theta_angle = theta*(1/(2**(n-1)))
-        gp_angle = angle(global_phase)*(1/(2**(n-1)))
-
-        for pattern in gray_code:
-            if '1' not in pattern:
-                continue
-            if last_pattern is None:
-                last_pattern = pattern
-            #find left most set bit
-            lm_pos = list(pattern).index('1')
-
-            #find changed bit
-            comp = [i != j for i, j in zip(pattern, last_pattern)]
-            if True in comp:
-                pos = comp.index(True)
-            else:
-                pos = None
-            if pos is not None:
-                if pos != lm_pos:
-                    circuit.cx(ctls[pos], ctls[lm_pos])
-                else:
-                    indices = [i for i, x in enumerate(pattern) if x == '1']
-                    for idx in indices[1:]:
-                        circuit.cx(ctls[idx], ctls[lm_pos])
-            #check parity
-            if pattern.count('1') % 2 == 0:
-                #inverse
-                apply_cu1(circuit, -theta_angle, ctls[lm_pos], tgt)
-                if global_phase:
-                    circuit.u1(-gp_angle, ctls[lm_pos])
-            else:
-                apply_cu1(circuit, theta_angle, ctls[lm_pos], tgt)
-                if global_phase:
-                    circuit.u1(gp_angle, ctls[lm_pos])
+    for pattern in gray_code:
+        if '1' not in pattern:
+            continue
+        if last_pattern is None:
             last_pattern = pattern
+        #find left most set bit
+        lm_pos = list(pattern).index('1')
 
+        #find changed bit
+        comp = [i != j for i, j in zip(pattern, last_pattern)]
+        if True in comp:
+            pos = comp.index(True)
+        else:
+            pos = None
+        if pos is not None:
+            if pos != lm_pos:
+                circuit.cx(ctls[pos], ctls[lm_pos])
+            else:
+                indices = [i for i, x in enumerate(pattern) if x == '1']
+                for idx in indices[1:]:
+                    circuit.cx(ctls[idx], ctls[lm_pos])
+        #check parity
+        if pattern.count('1') % 2 == 0:
+            #inverse
+            apply_cu1(circuit, -theta_angle, ctls[lm_pos], tgt)
+            if global_phase:
+                circuit.u1(-gp_angle, ctls[lm_pos])
+        else:
+            apply_cu1(circuit, theta_angle, ctls[lm_pos], tgt)
+            if global_phase:
+                circuit.u1(gp_angle, ctls[lm_pos])
+        last_pattern = pattern
 
 def mcu1(self, theta, control_qubits, target_qubit):
-    """Apply MCU1 to circuit."""
+    """
+    Apply Multiple-Controlled U1 gate
+    Args:
+        theta: angle theta
+        control_qubits: The list of control qubits
+        target_qubit: The target qubit
+    """
     if isinstance(target_qubit, QuantumRegister) and len(target_qubit) == 1:
         target_qubit = target_qubit[0]
     temp = []
@@ -102,7 +85,11 @@ def mcu1(self, theta, control_qubits, target_qubit):
     self._check_qubit(target_qubit)
     temp.append(target_qubit)
     self._check_dups(temp)
-    return self._attach(MCU1Gate(theta, control_qubits, target_qubit, self))
+    n_c = len(control_qubits)
+    if n_c == 1:  # cu1
+        apply_cu1(self, theta, control_qubits[0], target_qubit)
+    else:
+        _apply_mcu1(self, theta, control_qubits, target_qubit)
 
 
 QuantumCircuit.mcu1 = mcu1
