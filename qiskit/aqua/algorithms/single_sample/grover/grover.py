70d4bc5c1fc064e5aa9d34e09abc57c310efdfaa
Manoel Marques <manoel@us.ibm.com>
2019-02-12 12:10:49 -0500

    Fix Grover init_state num_qubits initialization

--- qiskit/aqua/algorithms/single_sample/grover/grover.py
+++ qiskit/aqua/algorithms/single_sample/grover/grover.py
@@ -180,7 +180,7 @@ class Grover(QuantumAlgorithm):
 
         # Set up initial state, we need to add computed num qubits to params
         init_state_params = params.get(Pluggable.SECTION_KEY_INITIAL_STATE)
-        init_state_params['num_qubits'] = len(oracle.variable_register)
+        init_state_params['num_qubits'] = len(oracle.variable_register) if oracle.variable_register else 0
         init_state = get_pluggable_class(PluggableType.INITIAL_STATE,
                                          init_state_params['name']).init_params(params)
 
