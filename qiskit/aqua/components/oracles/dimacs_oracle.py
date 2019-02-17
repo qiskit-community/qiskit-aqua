ddec59c918b2949f87c1fbb294e04b1784a6b609
Shaohan Hu <shaohan.hu@ibm.com>
2019-02-17 13:01:07 -0500

    rewrite dimacs oracle using pyeda

--- qiskit/aqua/components/oracles/dimacs_oracle.py
+++ qiskit/aqua/components/oracles/dimacs_oracle.py
@@ -20,8 +20,15 @@ The DIMACS-based Quantum Oracle.
 
 import logging
 
+from pyeda.inter import espresso_exprs
+from pyeda.boolalg.bfarray import exprvars
+from pyeda.boolalg.expr import ast2expr, AndOp, OrOp
+from pyeda.parsing.dimacs import parse_cnf
+from qiskit import QuantumRegister, QuantumCircuit
+
+from qiskit.aqua import AquaError
+from qiskit.aqua.utils import CNF, DNF
 from qiskit.aqua.components.oracles import Oracle
-from qiskit.aqua.utils import CNF
 
 logger = logging.getLogger(__name__)
 
@@ -39,6 +46,24 @@ class DimacsOracle(Oracle):
                 'dimacs_cnf': {
                     'type': 'string',
                 },
+                "optimization_mode": {
+                    "anyOf": [
+                        {
+                            "type": "null",
+                        },
+                        {
+                            "type": "string",
+                            "default": "espresso",
+                            'oneOf': [
+                                {
+                                    'enum': [
+                                        'espresso',
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
                 'mct_mode': {
                     'type': 'string',
                     'default': 'basic',
@@ -52,86 +77,87 @@ class DimacsOracle(Oracle):
                         }
                     ]
                 },
-                'optimization_mode': {
-                    'type': ['string', 'null'],
-                    'default': 'null',
-                    'oneOf': [
-                        {
-                            'enum': [
-                                'espresso',
-                            ]
-                        }
-                    ]
-                },
             },
             'additionalProperties': False
         }
     }
 
-    def __init__(self, dimacs_str=None, mct_mode='basic'):
+    def __init__(self, dimacs_str=None, optimization_mode=None, mct_mode='basic'):
         """
         Constructor.
 
         Args:
-            dimacs_str (str): The string representation in DIMACS format
-            mct_mode (str): The mode to use for building Multiple-Control Toffoli
+            dimacs_str (str): The string representation in DIMACS format.
+            optimization_mode (string): Optimization mode to use for minimizing the circuit.
+                Currently, besides no optimization if omitted, Aqua also supports an 'expresso' mode
+                <https://en.wikipedia.org/wiki/Espresso_heuristic_logic_minimizer>
+            mct_mode (str): The mode to use for building Multiple-Control Toffoli.
         """
 
         self.validate(locals())
         self._mct_mode = mct_mode
 
-        if dimacs_str:
-            ls = [
-                l.strip() for l in dimacs_str.split('\n')
-                if len(l) > 0 and not l.strip()[0] == 'c'
-            ]
-            headers = [l for l in ls if l[0] == 'p']
-            if len(headers) == 1:
-                p, sig, nv, nc = headers[0].split()
-                assert p == 'p' and sig == 'cnf'
-            else:
-                raise ValueError('Invalid cnf format for SAT.')
-            h_nv, h_nc = int(nv), int(nc)
-            cs = [
-                c.strip()
-                for c in ' '.join(
-                    [l for l in ls if not l[0] == 'p']
-                ).split(' 0') if len(c) > 0
-            ]
-            cnf_expr = [
-                [int(v) for v in c.split() if not int(v) == 0]
-                for c in cs
-                if (
-                    len(c.replace('0', '')) > 0
-                ) and (
-                    '0' <= c[0] <= '9' or c[0] == '-'
-                )
-            ]
-        else:
+        if dimacs_str is None:
             raise ValueError('Missing input DIMACS string.')
+        expr = ast2expr(parse_cnf(dimacs_str.strip()))
+        self._num_vars = expr.degree
 
-        self._cnf = CNF(cnf_expr)
+        if optimization_mode is None:
+            self._nf = CNF(expr, num_vars=self._num_vars, depth=2)
+        elif optimization_mode == 'espresso':
+            expr_dnf = expr.to_dnf()
+            if expr_dnf.is_zero() or expr_dnf.is_one():
+                self._nf = CNF(expr_dnf, num_vars=self._num_vars)
+            else:
+                expr_dnf_m = espresso_exprs(expr_dnf)[0]
+                if isinstance(expr_dnf_m, AndOp):
+                    self._nf = CNF(expr_dnf_m, num_vars=self._num_vars)
+                elif isinstance(expr_dnf_m, OrOp):
+                    self._nf = DNF(expr_dnf_m, num_vars=self._num_vars)
+                else:
+                    raise AquaError('Unexpected espresso optimization result expr: {}'.format(expr_dnf_m))
+
         super().__init__()
 
     @property
     def variable_register(self):
-        return self._cnf.variable_register
+        return self._variable_register
 
     @property
     def ancillary_register(self):
-        return self._cnf.ancillary_register
+        return self._ancillary_register
 
     @property
     def output_register(self):
-        return self._cnf.output_register
+        return self._output_register
 
     def construct_circuit(self):
-        return self._cnf.construct_circuit(mct_mode=self._mct_mode)
+        if self._circuit is None:
+            if self._nf is not None:
+                self._circuit = self._nf.construct_circuit(mct_mode=self._mct_mode)
+                self._variable_register = self._nf.variable_register
+                self._output_register = self._nf.output_register
+                self._ancillary_register = self._nf.ancillary_register
+            else:
+                self._variable_register = QuantumRegister(self._num_vars, name='v')
+                self._output_register = QuantumRegister(1, name='o')
+                self._ancillary_register = None
+                self._circuit = QuantumCircuit(self._variable_register, self._output_register)
+        return self._circuit
 
     def evaluate_classically(self, measurement):
         assignment = [(var + 1) * (int(tf) * 2 - 1) for tf, var in zip(measurement[::-1], range(len(measurement)))]
-        assignment_set = set(assignment)
-        for clause in self._cnf.expr:
-            if assignment_set.isdisjoint(clause):
-                return False, assignment
-        return True, assignment
+        if self._nf._expr.is_zero():
+            found = False
+        elif self._nf._expr.is_one():
+            found = True
+        else:
+            prime_implicants = self._nf._expr.complete_sum()
+            if isinstance(prime_implicants, AndOp):
+                sols = [[l[1] for l in prime_implicants.to_ast()[1:]]]
+            elif isinstance(prime_implicants, OrOp):
+                sols =[[l[1] for l in c[1:]] for c in self._nf._expr.complete_sum().to_ast()[1:]]
+            else:
+                raise AquaError('Unexpected solution: {}'.format(prime_implicants))
+            found = assignment in sols
+        return found, assignment
