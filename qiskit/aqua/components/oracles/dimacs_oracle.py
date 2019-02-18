6049c35a39d7a1f95f715a9413e89830d383c5ad
Shaohan Hu <shaohan.hu@ibm.com>
2019-02-18 17:40:19 -0500

    make dimacs oracle subclass logic expr oracle

--- qiskit/aqua/components/oracles/dimacs_oracle.py
+++ qiskit/aqua/components/oracles/dimacs_oracle.py
@@ -20,19 +20,16 @@ The DIMACS-based Quantum Oracle.
 
 import logging
 
-from pyeda.inter import espresso_exprs
-from pyeda.boolalg.expr import ast2expr, AndOp, OrOp
+from pyeda.boolalg.expr import ast2expr
 from pyeda.parsing.dimacs import parse_cnf
-from qiskit import QuantumRegister, QuantumCircuit
 
-from qiskit.aqua import AquaError
-from qiskit.aqua.utils import CNF, DNF
-from qiskit.aqua.components.oracles import Oracle
+from .logic_expr_oracle import LogicExpressionOracle
+from .oracle import Oracle
 
 logger = logging.getLogger(__name__)
 
 
-class DimacsOracle(Oracle):
+class DimacsOracle(LogicExpressionOracle):
 
     CONFIGURATION = {
         'name': 'DimacsOracle',
@@ -42,7 +39,7 @@ class DimacsOracle(Oracle):
             'id': 'dimacs_oracle_schema',
             'type': 'object',
             'properties': {
-                'dimacs_cnf': {
+                'dimacs_str': {
                     'type': 'string',
                 },
                 "optimization_mode": {
@@ -95,87 +92,13 @@ class DimacsOracle(Oracle):
 
         self.validate(locals())
         self._mct_mode = mct_mode
+        self._optimization_mode = optimization_mode
 
         if dimacs_str is None:
             raise ValueError('Missing input DIMACS string.')
-        ast = parse_cnf(dimacs_str.strip(), varname='v')
 
-        def move_vars_to_0_based(ast):
-            clauses = []
-            for c in ast[1:]:
-                clause = []
-                for l in c[1:]:
-                    if l[0] == 'not':
-                        new_lit = ('not', ('var', ('v',), (l[1][-1][0] - 1,),),)
-                    else:
-                        new_lit = ('var', ('v',), (l[-1][0] - 1,),)
-                    clause.append(new_lit)
-                # print('\t', c)
-                # print('\t', ('or', *tuple(clause)))
-                clauses.append(('or', *tuple(clause)))
-            return ('and', *tuple(clauses))
+        expr = ast2expr(parse_cnf(dimacs_str.strip(), varname='v'))
+        self._expr = expr
+        self._process_expr()
 
-        ast = move_vars_to_0_based(ast)
-
-        expr = ast2expr(ast)
-        self._num_vars = expr.degree
-
-        if optimization_mode is None:
-            self._nf = CNF(expr, num_vars=self._num_vars, depth=2)
-        elif optimization_mode == 'espresso':
-            expr_dnf = expr.to_dnf()
-            if expr_dnf.is_zero() or expr_dnf.is_one():
-                self._nf = CNF(expr_dnf, num_vars=self._num_vars)
-            else:
-                expr_dnf_m = espresso_exprs(expr_dnf)[0]
-                if isinstance(expr_dnf_m, AndOp):
-                    self._nf = CNF(expr_dnf_m, num_vars=self._num_vars)
-                elif isinstance(expr_dnf_m, OrOp):
-                    self._nf = DNF(expr_dnf_m, num_vars=self._num_vars)
-                else:
-                    raise AquaError('Unexpected espresso optimization result expr: {}'.format(expr_dnf_m))
-
-        super().__init__()
-
-    @property
-    def variable_register(self):
-        return self._variable_register
-
-    @property
-    def ancillary_register(self):
-        return self._ancillary_register
-
-    @property
-    def output_register(self):
-        return self._output_register
-
-    def construct_circuit(self):
-        if self._circuit is None:
-            if self._nf is not None:
-                self._circuit = self._nf.construct_circuit(mct_mode=self._mct_mode)
-                self._variable_register = self._nf.variable_register
-                self._output_register = self._nf.output_register
-                self._ancillary_register = self._nf.ancillary_register
-            else:
-                self._variable_register = QuantumRegister(self._num_vars, name='v')
-                self._output_register = QuantumRegister(1, name='o')
-                self._ancillary_register = None
-                self._circuit = QuantumCircuit(self._variable_register, self._output_register)
-        return self._circuit
-
-    def evaluate_classically(self, measurement):
-        assignment = [(var + 1) * (int(tf) * 2 - 1) for tf, var in zip(measurement[::-1], range(len(measurement)))]
-        if self._nf._expr.is_zero():
-            found = False
-        elif self._nf._expr.is_one():
-            found = True
-        else:
-            prime_implicants = self._nf._expr.complete_sum()
-            if isinstance(prime_implicants, AndOp):
-                sols = [[l[1] for l in prime_implicants.to_ast()[1:]]]
-            elif isinstance(prime_implicants, OrOp):
-                sols =[[l[1] for l in c[1:]] for c in self._nf._expr.complete_sum().to_ast()[1:]]
-            else:
-                raise AquaError('Unexpected solution: {}'.format(prime_implicants))
-            found = assignment in sols
-        return found, assignment
+        Oracle.__init__(self)
