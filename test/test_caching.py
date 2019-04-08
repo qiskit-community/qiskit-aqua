0abbbff99f0a6a65eed9323aa1cfb6097cad1335
Donny Greenberg <dongreenberg2@gmail.com>
2019-04-08 14:42:35 -0400

    Fine-grained tests for cache saving and loading, ensure tempfile deletion

--- test/test_caching.py
+++ test/test_caching.py
@@ -4,16 +4,19 @@ import numpy as np
 import os
 from parameterized import parameterized
 import tempfile
+import pickle
 
 from qiskit import BasicAer
 from test.common import QiskitAquaTestCase
 from qiskit.aqua import Operator, QuantumInstance, QiskitAqua
 from qiskit.aqua.input import EnergyInput
-from qiskit.aqua.components.variational_forms import RY
+from qiskit.aqua.components.variational_forms import RY, RYRZ
 from qiskit.aqua.components.optimizers import L_BFGS_B
 from qiskit.aqua.components.initial_states import Zero
 from qiskit.aqua.algorithms.adaptive import VQE
 from qiskit.aqua.utils import CircuitCache
+from qiskit.qobj import Qobj
+
 
 class TestCaching(QiskitAquaTestCase):
 
@@ -108,7 +111,7 @@ class TestCaching(QiskitAquaTestCase):
         speedup = result_caching['eval_time'] / self.reference_vqe_result['statevector_simulator']['eval_time']
         self.assertLess(speedup, speedup_min)
 
-    def test_saving_and_loading(self):
+    def test_saving_and_loading_e2e(self):
         backend = BasicAer.get_backend('statevector_simulator')
         num_qubits = self.algo_input.qubit_op.num_qubits
         init_state = Zero(num_qubits)
@@ -116,26 +119,65 @@ class TestCaching(QiskitAquaTestCase):
         optimizer = L_BFGS_B()
         algo = VQE(self.algo_input.qubit_op, var_form, optimizer, 'matrix')
 
-        fd, cache_tmp_file = tempfile.mkstemp(suffix='.inp')
-        os.close(fd)
+        with tempfile.NamedTemporaryFile(suffix='.inp', delete=True) as cache_tmp_file:
+            cache_tmp_file_name = cache_tmp_file.name
+            quantum_instance_caching = QuantumInstance(backend,
+                                                       circuit_caching=True,
+                                                       cache_file=cache_tmp_file_name,
+                                                       skip_qobj_deepcopy=True,
+                                                       skip_qobj_validation=True)
+            algo.run(quantum_instance_caching)
+            self.assertLessEqual(quantum_instance_caching.circuit_cache.misses, 0)
 
-        quantum_instance_caching = QuantumInstance(backend,
-                                                   circuit_caching=True,
-                                                   cache_file=cache_tmp_file,
-                                                   skip_qobj_deepcopy=True,
-                                                   skip_qobj_validation=True)
-        algo.run(quantum_instance_caching)
-        self.assertLessEqual(quantum_instance_caching.circuit_cache.misses, 0)
+            is_file_exist = os.path.exists(cache_tmp_file_name)
+            self.assertTrue(is_file_exist, "Does not store content successfully.")
 
-        is_file_exist = os.path.exists(cache_tmp_file)
-        self.assertTrue(is_file_exist, "Does not store content successfully.")
+            circuit_cache_new = CircuitCache(skip_qobj_deepcopy=True, cache_file=cache_tmp_file_name)
+            self.assertEqual(quantum_instance_caching.circuit_cache.mappings, circuit_cache_new.mappings)
+            self.assertLessEqual(circuit_cache_new.misses, 0)
 
-        circuit_cache_new = CircuitCache(skip_qobj_deepcopy=True, cache_file=cache_tmp_file)
-        self.assertEqual(quantum_instance_caching.circuit_cache.mappings, circuit_cache_new.mappings)
-        self.assertLessEqual(circuit_cache_new.misses, 0)
+    def test_saving_and_loading_one_circ(self):
+        with tempfile.NamedTemporaryFile(suffix='.inp', delete=True) as cache_tmp_file:
+            cache_tmp_file_name = cache_tmp_file.name
+            var_form = RYRZ(num_qubits=4, depth=5)
+            backend = BasicAer.get_backend('statevector_simulator')
 
-        if is_file_exist:
-            os.remove(cache_tmp_file)
+            params0 = np.random.random(var_form.num_parameters)
+            circ0 = var_form.construct_circuit(params0)
+
+            quantum_instance0 = QuantumInstance(backend,
+                                                       circuit_caching=True,
+                                                       cache_file=cache_tmp_file_name,
+                                                       skip_qobj_deepcopy=True,
+                                                       skip_qobj_validation=True)
+
+            result0 = quantum_instance0.execute([circ0])
+            cache_handler = open(cache_tmp_file_name, "rb")
+            saved_cache = pickle.load(cache_handler, encoding="ASCII")
+            self.assertIn('qobjs', saved_cache)
+            self.assertIn('mappings', saved_cache)
+            qobjs = [Qobj.from_dict(qob) for qob in saved_cache['qobjs']]
+            self.assertTrue(isinstance(qobjs[0], Qobj))
+            self.assertGreaterEqual(len(saved_cache['mappings'][0][0]), 50)
+
+            quantum_instance1 = QuantumInstance(backend,
+                                               circuit_caching=True,
+                                               cache_file=cache_tmp_file_name,
+                                               skip_qobj_deepcopy=True,
+                                               skip_qobj_validation=True)
+
+            params1 = np.random.random(var_form.num_parameters)
+            circ1 = var_form.construct_circuit(params1)
+
+            qobj1 = quantum_instance1.circuit_cache.load_qobj_from_cache([circ1], 0,
+                                                                         run_config=quantum_instance1.run_config)
+            self.assertTrue(isinstance(qobj1, Qobj))
+            result1 = quantum_instance1.execute([circ1])
+
+            self.assertEqual(quantum_instance0.circuit_cache.mappings, quantum_instance1.circuit_cache.mappings)
+            self.assertLessEqual(quantum_instance1.circuit_cache.misses, 0)
+
+
 
 
 if __name__ == '__main__':
