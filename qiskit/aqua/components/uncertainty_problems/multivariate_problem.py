73a4e95f2faef2d1805da1270efde1c92c799d40
stefan-woerner <stefan@swoerner.de>
2019-04-17 10:33:13 +0200

    Update multivariate_problem.py

--- qiskit/aqua/components/uncertainty_problems/multivariate_problem.py
+++ qiskit/aqua/components/uncertainty_problems/multivariate_problem.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 # =============================================================================
 from qiskit.aqua.components.uncertainty_problems import UncertaintyProblem
+from qiskit.aqua.circuits.gates import mct
 import numpy as np
 
 
@@ -38,8 +39,8 @@ class MultivariateProblem(UncertaintyProblem):
         super().__init__(num_target_qubits)
 
         # set qubit indices for
-        self.i_state = list(range(num_target_qubits-1))
-        self.i_objective = num_target_qubits-1
+        self.i_state = list(range(num_target_qubits - 1))
+        self.i_objective = num_target_qubits - 1
 
         # store parameters
         self._uncertainty_model = uncertainty_model
@@ -55,61 +56,74 @@ class MultivariateProblem(UncertaintyProblem):
 
     def required_ancillas(self):
 
-        num_uncertainty_ancillas = self._uncertainty_model.required_ancillas()
-        num_aggregation_ancillas = self._aggregation_function.num_sum_qubits + self._aggregation_function.required_ancillas()
-        num_objective_ancillas = self._univariate_objective.required_ancillas()
         num_condition_ancillas = 0
-
         num_condition_target_ancillas = 0
-        if self._conditions is not None and len(self._conditions) > 1:
-            num_condition_target_ancillas += 1
+        num_aggregation_ancillas = self._aggregation_function.required_ancillas()
+        if self._conditions is not None:
+            num_condition_target_ancillas = len(self._conditions) + 1*(len(self._conditions) > 1)
+            num_aggregation_ancillas = self._aggregation_function.required_ancillas_controlled()
         if self._conditions is not None:
             for (dim, condition) in self._conditions:
-                num_condition_ancillas = np.maximum(num_condition_ancillas, num_condition_target_ancillas + condition.num_target_qubits + condition.required_ancillas())
+                num_condition_ancillas = np.maximum(num_condition_ancillas, condition.required_ancillas())
 
-        # aggregation, condition, and objective ancillas are added, since aggregation is only uncomputed AFTER objective evaluation
-        return max([num_uncertainty_ancillas, num_aggregation_ancillas + num_objective_ancillas + num_condition_ancillas])
+        # get maximal number of required ancillas
+        num_ancillas = max([self._uncertainty_model.required_ancillas(),
+                            num_aggregation_ancillas,
+                            self._univariate_objective.required_ancillas(),
+                            num_condition_ancillas])
+
+        # add ancillas that are required to compute intermediate states are are no directly uncomputed
+        num_ancillas += self._aggregation_function.num_sum_qubits
+        num_ancillas += num_condition_target_ancillas
+
+        return num_ancillas
 
     def build(self, qc, q, q_ancillas=None):
 
         # apply uncertainty model (can use all ancillas and returns all clean)
-        self._uncertainty_model.build(qc, q, q_ancillas)
+        q_state = [q[i] for i in self.i_state]
+        self._uncertainty_model.build(qc, q_state, q_ancillas)
 
         qc.barrier()
+
+        # get all qubits up to the largest state qubit
+        num_agg_qubits = self._aggregation_function.num_sum_qubits
+        q_agg_in = q_state
+        q_agg_out = [q_ancillas[i] for i in range(num_agg_qubits)]
+        q_agg = q_agg_in + q_agg_out
+
+        # determine objective qubits (aggregation qubits + objective qubit)
+        q_obj = q_agg_out + [q[self.i_objective]]
+
+        # set condition target qubits
+        if self._conditions is not None and len(self._conditions) > 0:
+            i_cond_start = num_agg_qubits
+            i_cond_end = i_cond_start + len(self._conditions) + 1*(len(self._conditions) > 1)
+            q_cond_target = [q_ancillas[i] for i in range(i_cond_start, i_cond_end)]
+
+            # set remaining ancillas
+            remaining_ancillas_start = i_cond_end
+        else:
+            # set remaining ancillas
+            remaining_ancillas_start = num_agg_qubits
+
+        q_rem_ancillas = [q_ancillas[i] for i in range(remaining_ancillas_start, len(q_ancillas))]
 
         # apply controlled or uncontrolled aggregation
         if self._conditions is None or len(self._conditions) == 0:
 
-            # get all qubits up to the largest state qubit
-            num_agg_qubits = self._aggregation_function.num_sum_qubits
-            q_agg_in = q[self.i_state]
-            q_agg_out = q_ancillas[[i for i in range(num_agg_qubits)]]
-            q_agg = q_agg_in + q_agg_out
-
-            # add required ancillas to auxilliary ancilla register
-            i_agg_ancillas_start = self._aggregation_function.num_sum_qubits
-            i_agg_ancillas_end = i_agg_ancillas_start + self._aggregation_function.required_ancillas()
-            q_agg_ancillas = [q_ancillas[i] for i in range(i_agg_ancillas_start, i_agg_ancillas_end)]
-
-            # determine objective qubits (aggregation qubits + objective qubit)
-            q_obj = q_agg_out + [q[self.i_objective]]
-
-            # determine remaining ancillas for objective (returns them clean)
-            i_obj_ancillas_end = i_agg_ancillas_end + self._univariate_objective.required_ancillas()
-            q_obj_ancillas = [q_ancillas[i] for i in range(i_agg_ancillas_end, i_obj_ancillas_end)]
-
             # apply aggregation
-            self._aggregation_function.build(qc, q_agg, q_agg_ancillas)
+            self._aggregation_function.build(qc, q_agg, q_rem_ancillas)
 
             qc.barrier()
 
             # apply objective function
-            self._univariate_objective.build(qc, q_obj, q_obj_ancillas)
+            self._univariate_objective.build(qc, q_obj, q_rem_ancillas)
 
             qc.barrier()
 
             # uncompute aggregation (all ancillas should be clean again now)
-            self._aggregation_function.build_inverse(qc, q_agg, q_agg_ancillas)
+            self._aggregation_function.build_inverse(qc, q_agg, q_rem_ancillas)
 
             qc.barrier()
 
@@ -117,70 +131,83 @@ class MultivariateProblem(UncertaintyProblem):
 
             if len(self._conditions) == 1:
 
-                # get all qubits up to the largest state qubit
-                num_agg_qubits = self._aggregation_function.num_sum_qubits
-                q_agg_in = q[self.i_state]
-                q_agg_out = q_ancillas[[i for i in range(num_agg_qubits)]]
-                q_agg = q_agg_in + q_agg_out
-
-                # add required ancillas to auxilliary ancilla register
-                i_agg_ancillas_start = self._aggregation_function.num_sum_qubits
-                i_agg_ancillas_end = i_agg_ancillas_start + self._aggregation_function.required_ancillas_controlled()
-                q_agg_ancillas = [q_ancillas[i] for i in range(i_agg_ancillas_start, i_agg_ancillas_end)]
-
-                # determine objective qubits (aggregation qubits + objective qubit)
-                q_obj = q_agg_out + [q[self.i_objective]]
-
-                # determine remaining ancillas for objective (returns them clean)
-                i_obj_ancillas_end = i_agg_ancillas_end + self._univariate_objective.required_ancillas()
-                q_obj_ancillas = [q_ancillas[i] for i in range(i_agg_ancillas_end, i_obj_ancillas_end)]
-
                 dimension = self._conditions[0][0]
                 condition = self._conditions[0][1]
 
-                i_condition_in_end = i_obj_ancillas_end+self._uncertainty_model.num_qubits[dimension]
-                q_condition_in = q_ancillas[i_obj_ancillas_end:i_condition_in_end]
-                q_condition_out = q_ancillas[i_condition_in_end]
-                q_condition = q_condition_in + [q_condition_out]
+                i_condition_in_start = np.cumsum(self._uncertainty_model.num_qubits)[dimension] - self._uncertainty_model.num_qubits[dimension]
+                i_condition_in_end = np.cumsum(self._uncertainty_model.num_qubits)[dimension]
+                q_condition_in = [q_state[i] for i in range(i_condition_in_start, i_condition_in_end)]
 
-                i_cond_ancillas_start = i_condition_in_end + 1
-                i_cond_ancillas_end = i_cond_ancillas_start + condition.required_ancillas()
-                q_condition_ancillas = q_ancillas[i_cond_ancillas_start:i_cond_ancillas_end]
+                q_condition = q_condition_in + [q_cond_target[0]]
 
-                condition.build(qc, q_condition, q_condition_ancillas)
+                condition.build(qc, q_condition, q_rem_ancillas)
 
                 qc.barrier()
 
                 # apply aggregation
-                self._aggregation_function.build_controlled(qc, q_agg, q_condition_out, q_agg_ancillas)
+                self._aggregation_function.build_controlled(qc, q_agg, q_cond_target[0], q_rem_ancillas, use_basis_gates=False)
 
                 qc.barrier()
 
                 # apply objective function
-                self._univariate_objective.build(qc, q_obj, q_obj_ancillas)
+                self._univariate_objective.build(qc, q_obj, q_rem_ancillas)
 
                 qc.barrier()
 
                 # uncompute aggregation (all ancillas should be clean again now)
-                self._aggregation_function.build_controlled_inverse(qc, q_agg, q_condition_out, q_agg_ancillas)
+                self._aggregation_function.build_controlled_inverse(qc, q_agg, q_cond_target[0], q_rem_ancillas, use_basis_gates=False)
 
                 qc.barrier()
 
                 # uncompute condition
-                condition.build_inverse(qc, q_condition, q_condition_ancillas)
+                condition.build_inverse(qc, q_condition, q_rem_ancillas)
 
             else:
 
-                pass
-                # TODO: needs to be implemented
-                # # combine conditions in ancilla
-                # qc.mct([q[i] for i in i_condition], q_ancillas[-1], None)
-                #
-                # # aggregate results controlled with ancilla
-                # self._aggregation_function.build_controlled(qc, q, q_ancillas[-1], q_ancillas)
-                #
-                # # uncompute conditions in ancilla
-                # qc.mct([q[i] for i in i_condition], q_ancillas[-1], None)
+                for j in range(len(self._conditions)):
 
+                    dimension = self._conditions[j][0]
+                    condition = self._conditions[j][1]
 
+                    i_condition_in_start = np.cumsum(self._uncertainty_model.num_qubits)[dimension] - self._uncertainty_model.num_qubits[dimension]
+                    i_condition_in_end = np.cumsum(self._uncertainty_model.num_qubits)[dimension]
+                    q_condition_in = [q_state[i] for i in range(i_condition_in_start, i_condition_in_end)]
 
+                    q_condition = q_condition_in + [q_cond_target[j]]
+
+                    condition.build(qc, q_condition, q_rem_ancillas)
+
+                qc.mct(q_cond_target[:-1], q_cond_target[-1], q_rem_ancillas)
+
+                qc.barrier()
+
+                # apply aggregation
+                self._aggregation_function.build_controlled(qc, q_agg, q_cond_target[-1], q_rem_ancillas, use_basis_gates=False)
+
+                qc.barrier()
+
+                # apply objective function
+                self._univariate_objective.build(qc, q_obj, q_rem_ancillas)
+
+                qc.barrier()
+
+                # uncompute aggregation (all ancillas should be clean again now)
+                self._aggregation_function.build_controlled_inverse(qc, q_agg, q_cond_target[-1], q_rem_ancillas, use_basis_gates=False)
+
+                qc.barrier()
+
+                qc.mct(q_cond_target[:-1], q_cond_target[-1], q_rem_ancillas)
+
+                # uncompute condition
+                for j in range(len(self._conditions)):
+
+                    dimension = self._conditions[j][0]
+                    condition = self._conditions[j][1]
+
+                    i_condition_in_start = np.cumsum(self._uncertainty_model.num_qubits)[dimension] - self._uncertainty_model.num_qubits[dimension]
+                    i_condition_in_end = np.cumsum(self._uncertainty_model.num_qubits)[dimension]
+                    q_condition_in = [q_state[i] for i in range(i_condition_in_start, i_condition_in_end)]
+
+                    q_condition = q_condition_in + [q_cond_target[j]]
+
+                    condition.build_inverse(qc, q_condition, q_rem_ancillas)
