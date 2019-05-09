f3590a3d93e4cfa689250b3b328997a00cbe1bf5
Shaohan Hu <shaohan.hu@ibm.com>
2019-05-09 14:01:44 -0400

    minor rename

--- qiskit/aqua/circuits/gates/multi_control_toffoli_gate.py
+++ qiskit/aqua/circuits/gates/multi_control_toffoli_gate.py
@@ -28,7 +28,7 @@ from .relative_phase_toffoli import rccx
 logger = logging.getLogger(__name__)
 
 
-def _ccx_v_chain(qc, control_qubits, target_qubit, ancillary_qubits, dirty_ancilla=False):
+def _mct_v_chain(qc, control_qubits, target_qubit, ancillary_qubits, dirty_ancilla=False):
     """
     Create new MCT circuit by chaining ccx gates into a V shape.
 
@@ -271,9 +271,9 @@ def mct(self, q_controls, q_target, q_ancilla, mode='basic'):
         self._check_dups(all_qubits)
 
         if mode == 'basic':
-            _ccx_v_chain(self, control_qubits, target_qubit, ancillary_qubits, dirty_ancilla=False)
+            _mct_v_chain(self, control_qubits, target_qubit, ancillary_qubits, dirty_ancilla=False)
         elif mode == 'basic-dirty-ancilla':
-            _ccx_v_chain(self, control_qubits, target_qubit, ancillary_qubits, dirty_ancilla=True)
+            _mct_v_chain(self, control_qubits, target_qubit, ancillary_qubits, dirty_ancilla=True)
         elif mode == 'advanced':
             _multicx(self, [*control_qubits, target_qubit], ancillary_qubits[0] if ancillary_qubits else None)
         elif mode == 'noancilla':
