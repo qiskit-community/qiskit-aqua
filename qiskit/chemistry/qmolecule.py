cf1f98cd428aab48af7c7a8463c1bb496c580297
Manoel Marques <manoel@us.ibm.com>
2019-08-26 20:08:40 -0400

    Fix lint

--- qiskit/chemistry/qmolecule.py
+++ qiskit/chemistry/qmolecule.py
@@ -501,13 +501,15 @@ class QMolecule:
         return moh2_qubit
 
     symbols = [
-        # pylint: disable=bad-whitespace,line-too-long
+        # pylint: disable=bad-whitespace
         '_',
         'H',  'He',
         'Li', 'Be', 'B',  'C',  'N',  'O',  'F',  'Ne',
         'Na', 'Mg', 'Al', 'Si', 'P',  'S',  'Cl', 'Ar',
-        'K',  'Ca', 'Sc', 'Ti', 'V',  'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
-        'Rb', 'Sr', 'Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
+        'K',  'Ca', 'Sc', 'Ti', 'V',  'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga',
+        'Ge', 'As', 'Se', 'Br', 'Kr',
+        'Rb', 'Sr', 'Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In',
+        'Sn', 'Sb', 'Te', 'I', 'Xe',
         'Cs', 'Ba',
         'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
         'Hf', 'Ta', 'W',  'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
