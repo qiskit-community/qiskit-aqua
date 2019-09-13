2f44f7c6a75deac8ab7e4ddc06c029a7c6804ca9
mrossinek <rmax@ethz.ch>
2019-09-13 23:40:44 +0200

    Debug VQEAdapt implementation.
    This introduces a number of debugging changes. A few notable changes are
    listed below:
    - excitations are properly inserted into all internal lists of var_form
    - the number of parameters and the bounds are updated accordingly
    - the circuit_name_prefixes are set properly (see documentation of the
    evaluate_with_result method in the WeightedPauliOperator class)
    - the correct operator_modes are used properly (includes usage of
    _config_the_best_mode function (copied from VQE class))
    - the parameter arrays are parsed consistently as python lists

--- qiskit/aqua/algorithms/adaptive/vqe_adapt/vqe_adapt.py
+++ qiskit/aqua/algorithms/adaptive/vqe_adapt/vqe_adapt.py
@@ -13,13 +13,18 @@
 # that they have been altered from the originals.
 
 import logging
+import functools
 import warnings
+
+import numpy as np
 
 from qiskit.aqua import Operator
 from qiskit.aqua.algorithms.adaptive.vq_algorithm import VQAlgorithm
 from qiskit.aqua.algorithms.adaptive.vqe.vqe import VQE
-from qiskit.chemistry.aqua_extensions.components.variational_forms.ucc import UCC
-from qiskit.aqua.operators import op_converter
+from qiskit.chemistry.aqua_extensions.components.variational_forms.ucc_new import UCCSD_single_operator
+from qiskit.aqua.operators import (TPBGroupedWeightedPauliOperator, WeightedPauliOperator,
+                                   MatrixOperator, op_converter)
+from qiskit.aqua.utils.backend_utils import is_aer_statevector_backend, is_statevector_backend
 
 logger = logging.getLogger(__name__)
 
@@ -57,27 +62,38 @@ class VQEAdapt(VQAlgorithm):
                           "it will be removed after 0.6.", DeprecationWarning)
             operator = op_converter.to_weighted_pauli_operator(operator)
         self._operator = operator
-        if not isinstance(var_form_base, UCC):
-            warnings.warn("var_form_base has to be an instance of UCC.")
+        if not isinstance(var_form_base, UCCSD_single_operator):
+            warnings.warn("var_form_base has to be an instance of UCCSD_single_operator.")
             return 1
         self._var_form_base = var_form_base
-        self._excitation_pool = self._var_form_base._hopping_ops
+        self._excitation_pool = excitation_pool
         self._threshold = threshold
 
-    def _compute_gradients(self,var_form, delta, optimizer, theta):
+    def _compute_gradients(self, var_form, delta, optimizer, theta, excitation_pool, operator):
 
         res = []
 
         for exc in excitation_pool:
-	    var_from._hoppin_ops.append(exc)
+            var_form._hopping_ops.append(exc)
+            var_form._num_parameters += 1
+            if exc._paulis[0][1].numberofqubits == 2:
+                var_form._single_excitations.append(exc)
+            elif exc._paulis[0][1].numberofqubits == 4:
+                var_form._double_excitations.append(exc)
+
             vqe = VQE(operator, var_form, optimizer)
 
-            qubit_op_and_param = (exc, delta)
+            if theta == []:
+                params_minus = [-delta]
+            else:
+                params_minus = theta + [-delta]
+            circuit_minus = vqe.construct_circuit(params_minus,
+                                                  statevector_mode=self._quantum_instance.is_statevector,
+                                                  use_simulator_operator_mode=self._use_simulator_operator_mode,
+                                                  circuit_name_prefix='minus')
 
-            circuit = vqe.construct_circuit(theta.append(-delta), statevector_mode=self._quantum_instance.is_statevector,
-                                         use_simulator_operator_mode=self._use_simulator_operator_mode,
-                                         circuit_name_prefix=str(idx))
-            to_be_simulated_circuit = functools.reduce(lambda x, y: x + y, circuit)
+            to_be_simulated_circuit_minus = functools.reduce(lambda x, y: x + y, circuit_minus)
+
             if self._use_simulator_operator_mode:
                 extra_args = {'expectation': {
                     'params': [self._operator.aer_paulis],
@@ -85,17 +101,22 @@ class VQEAdapt(VQAlgorithm):
                 }
             else:
                 extra_args = {}
-            result = self._quantum_instance.execute(to_be_simulated_circuit, **extra_args)
+            result_minus = self._quantum_instance.execute(to_be_simulated_circuit_minus, **extra_args)
 
-            mean_minus, std = self._operator.evaluate_with_result(
-                result=result, statevector_mode=self._quantum_instance.is_statevector,
-                use_simulator_operator_mode=self._use_simulator_operator_mode, circuit_name_prefix=str(idx))
+            mean_minus, std_minus = self._operator.evaluate_with_result(
+                    result=result_minus, statevector_mode=self._quantum_instance.is_statevector,
+                    use_simulator_operator_mode=self._use_simulator_operator_mode,
+                    circuit_name_prefix='minus')
 
-            theta.pop()
-            circuit = vqe.construct_circuit([theta.append(delta), statevector_mode=self._quantum_instance.is_statevector,
-                                         use_simulator_operator_mode=self._use_simulator_operator_mode,
-                                         circuit_name_prefix=str(idx))
-            to_be_simulated_circuit = functools.reduce(lambda x, y: x + y, circuit)
+            if theta == []:
+                params_plus = [delta]
+            else:
+                params_plus = theta + [delta]
+            circuit_plus = vqe.construct_circuit(params_plus,
+                                                 statevector_mode=self._quantum_instance.is_statevector,
+                                                 use_simulator_operator_mode=self._use_simulator_operator_mode,
+                                                 circuit_name_prefix='plus')
+            to_be_simulated_circuit_plus = functools.reduce(lambda x, y: x + y, circuit_plus)
             if self._use_simulator_operator_mode:
                 extra_args = {'expectation': {
                     'params': [self._operator.aer_paulis],
@@ -103,14 +124,20 @@ class VQEAdapt(VQAlgorithm):
                 }
             else:
                 extra_args = {}
-            result = self._quantum_instance.execute(to_be_simulated_circuit, **extra_args)
+            result_plus = self._quantum_instance.execute(to_be_simulated_circuit_plus, **extra_args)
 
-            mean_plus, std = self._operator.evaluate_with_result(
-                result=result, statevector_mode=self._quantum_instance.is_statevector,
-                use_simulator_operator_mode=self._use_simulator_operator_mode, circuit_name_prefix=str(idx))
-            theta.pop()
+            mean_plus, std_plus = self._operator.evaluate_with_result(
+                result=result_plus, statevector_mode=self._quantum_instance.is_statevector,
+                use_simulator_operator_mode=self._use_simulator_operator_mode,
+                circuit_name_prefix='plus')
 
             res.append(((mean_minus-mean_plus)/(2*delta), exc))
+            var_form._hopping_ops.pop()
+            var_form._num_parameters -= 1
+            if exc._paulis[0][1].numberofqubits == 2:
+                var_form._single_excitations.pop()
+            elif exc._paulis[0][1].numberofqubits == 4:
+                var_form._double_excitations.pop()
 
         return res
 
@@ -150,23 +177,39 @@ class VQEAdapt(VQAlgorithm):
         Raises:
             AquaError: wrong setting of operator and backend.
         """
+        self._operator = self._config_the_best_mode(self._operator, self._quantum_instance.backend)
+        self._use_simulator_operator_mode = \
+            is_aer_statevector_backend(self._quantum_instance.backend) \
+            and isinstance(self._operator, (WeightedPauliOperator, TPBGroupedWeightedPauliOperator))
+        self._quantum_instance.circuit_summary = True
+
         threshold_satisfied = False
         theta = []
+        iteration = 0
         while not threshold_satisfied:
+            iteration += 1
             # compute gradients
-            cur_grads = self._compute_gradients()
+            cur_grads = self._compute_gradients(self._var_form_base, 0.0001, self._optimizer, theta,
+                                                self._excitation_pool, self._operator)
             # pick maximum gradients and choose that excitation
-            max_grad = max(cur_grads, key=lambda item: item[1])
-            if max_grad[1] < self._threshold:
+            max_grad = max(cur_grads, key=lambda item: np.abs(item[0]))
+            if np.abs(max_grad[0]) < self._threshold:
                 threshold_satisfied = True
                 break
             # add new excitation to self._var_form_base
-            self._var_form_base = self._var_form_base  # .var_form_function(max_grad[0])
+            self._var_form_base._hopping_ops.append(max_grad[1])
+            if max_grad[1]._paulis[0][1].numberofqubits == 2:
+                self._var_form_base._single_excitations.append(max_grad[1])
+            elif max_grad[1]._paulis[0][1].numberofqubits == 4:
+                self._var_form_base._double_excitations.append(max_grad[1])
             theta.append(0.0)
+            self._var_form_base._num_parameters += 1
+            self._var_form_base._bounds = [(-np.pi, np.pi) for _ in range(self._var_form_base._num_parameters)]
             # run VQE on current Ansatz
             algorithm = VQE(self._operator, self._var_form_base, self._optimizer, initial_point=theta)
             self._ret = algorithm.run(self._quantum_instance)
-            theta = self._ret['eigvals']
+            theta = self._ret['opt_params'].tolist()
+            print('Iteration #' + str(iteration) + ': E = ' + str(self._ret['energy']))
         return self._ret
 
     def get_optimal_cost(self):
