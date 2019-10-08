6b642a0656416b1a3f60814cb49995957b061a9c
ebaeumer <48065247+ebaeumer@users.noreply.github.com>
2019-10-09 01:25:05 +0200

    even more style improvements

--- qiskit/aqua/algorithms/parallel/vqe_singlechip.py
+++ qiskit/aqua/algorithms/parallel/vqe_singlechip.py
@@ -1,21 +1,14 @@
-from qiskit.aqua.algorithms import VQE, ExactEigensolver
-import matplotlib.pyplot as plt
 import numpy as np
 import math
-import pickle
-from qiskit.aqua.components.variational_forms import RYRZ, VariationalForm
-from qiskit.aqua.components.optimizers import COBYLA, SPSA, SLSQP, AQGD
-from qiskit.chemistry.drivers import PySCFDriver, UnitsType
-from qiskit.chemistry import FermionicOperator
 from qiskit import IBMQ, Aer, execute, QuantumRegister, ClassicalRegister, QuantumCircuit
 from qiskit.providers.aer import noise
 from qiskit.aqua import QuantumInstance
 from qiskit.quantum_info import Pauli
 from qiskit.aqua.operators.weighted_pauli_operator import WeightedPauliOperator
 
-#backend = Aer.get_backend("qasm_simulator")
 
-def run(operator, optimizer, varform, backend, num_qubits, initialParameters=None, shotsPerPoint=10000):
+def run(operator, optimizer, varform, chosen_backend, num_qubits, initialParameters=None,
+        shotsPerPoint=10000):
     global shots
     global values
     global plottingTime
@@ -23,7 +16,7 @@ def run(operator, optimizer, varform, backend, num_qubits, initialParameters=Non
     global qr_size
     global var_form
     global backend
-    backend = backend
+    backend = chosen_backend
     var_form = varform
     qr_size = num_qubits
     plottingTime = True
@@ -41,7 +34,7 @@ def run(operator, optimizer, varform, backend, num_qubits, initialParameters=Non
 
 
 # Create Parallelized Measurement Circuits from single measurement circuits
-def opToCircs (circuit = QuantumCircuit, operator = WeightedPauliOperator, qr_size = int):
+def opToCircs (circuit=QuantumCircuit, operator=WeightedPauliOperator, qr_size=int):
     if(qr_size < operator.num_qubits):
         raise Exception('Error: Not enough qubits, enter at least QubitOp.num_qubits qubits.')
     qr = []
@@ -49,27 +42,28 @@ def opToCircs (circuit = QuantumCircuit, operator = WeightedPauliOperator, qr_si
     for i in range(len(operator.paulis)):
         qr.append(QuantumRegister(operator.num_qubits))
         cr.append(ClassicalRegister(operator.num_qubits))
-    meascircuits = operator.construct_evaluation_circuit(circuit, statevector_mode=False,
-                                                        qr=None, cr=None, use_simulator_operator_mode=False,
-                                                        circuit_name_prefix='')
+    meascircuits = operator.construct_evaluation_circuit(circuit, statevector_mode=False, qr=None,
+                                                         cr=None, use_simulator_operator_mode=False,
+                                                         circuit_name_prefix='')
     paulis_per_register = math.floor(qr_size/operator.num_qubits)
     numRegisters = math.ceil(len(operator.paulis)/paulis_per_register)
     output_circ = []
     for j in range(numRegisters):
-        l = j*paulis_per_register
+        m = j*paulis_per_register
         for k in range(paulis_per_register-1):
-            if(l+k+1<len(qr)):
-                meascircuits[l].add_register(qr[j*(paulis_per_register-1)+k+1], cr[j*(paulis_per_register-1)+k+1])
-                meascircuits[l].append(meascircuits[l+k+1].to_instruction(),
-                                       qr[j*(paulis_per_register-1)+k+1], cr[j*(paulis_per_register-1)+k+1])
-        output_circ.append(meascircuits[l].decompose())
+            if(m+k+1 < len(qr)):
+                meascircuits[m].add_register(qr[j*(paulis_per_register-1)+k+1],
+                                             cr[j*(paulis_per_register-1)+k+1])
+                meascircuits[m].append(meascircuits[m+k+1].to_instruction(),
+                                       qr[j*(paulis_per_register-1)+k+1],
+                                       cr[j*(paulis_per_register-1)+k+1])
+        output_circ.append(meascircuits[m].decompose())
     return output_circ
 
 
 # Function that is used by the optimizer, takes parameters and returns respective energy
 def energy_opt(parameters):
-    # Create variational form with RYRZ model and build corresponding circuit with parameters
-    #var_form = RYRZ(qubitOp.num_qubits, depth=3, entanglement="linear")
+    # Build circuit with parameters corresponding to the variational form
     circuit = var_form.construct_circuit(parameters)
 
     # Calculate and output the energy
@@ -78,7 +72,6 @@ def energy_opt(parameters):
         values.append(energy)
     #print(energy)
     return energy
-
 
 
 # Function that calculates energy
@@ -114,22 +107,23 @@ def E(circuit=QuantumCircuit, qubitOp=WeightedPauliOperator, qr_size=int):
         for i in range(len(sep_counts[0])-1):
             newdict = {}
             for j in range(2**pauli_size):
-                b ='{0:b}'.format(j)
+                b = '{0:b}'.format(j)
                 b = b.zfill(pauli_size)
-                newdict[b]=0
+                newdict[b] = 0
             for k in range(len(sep_counts)):
                 newdict[sep_counts[k][len(sep_counts[0])-2-i]] += sep_counts[k][-1]
-            energy += qubitOp.paulis[counter*paulis_per_register+i][0] \
-                     * sum_binary(newdict, qubitOp.paulis[counter*paulis_per_register+i][1])
+            energy += qubitOp.paulis[counter*paulis_per_register+i][0] * \
+                      sum_binary(newdict, qubitOp.paulis[counter*paulis_per_register+i][1])
         counter += 1
     return energy
 
+
 # Calculate the energy for a given Pauli operator and measurement outcome given as bitstring
-def sum_binary(counts, pauli = Pauli):
+def sum_binary(counts, pauli=Pauli):
     sum = 0
     total = 0
     # countOperator tracks which parts of the Pauli Operator consist of the identity (which does not affect the parity)
-    countOperator =  list(np.logical_or(pauli.x, pauli.z))
+    countOperator = list(np.logical_or(pauli.x, pauli.z))
     for key in counts:
         parity = 0
         counter = 0
@@ -141,8 +135,9 @@ def sum_binary(counts, pauli = Pauli):
         total += counts[key]
     return sum / total
 
+
 global plottingTime
-plottingTime= False
+plottingTime = False
 noisy = False
 
 # Noisy Backend
