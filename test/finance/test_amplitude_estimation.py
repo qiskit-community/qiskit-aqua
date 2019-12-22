eef5a2a6b68fac77bbaeffbc5460c5db8d996036
Cryoris <jules.gacon@googlemail.com>
2019-12-22 14:20:11 +0100

    fix expected value after MLAE fix

--- test/finance/test_amplitude_estimation.py
+++ test/finance/test_amplitude_estimation.py
@@ -25,13 +25,14 @@ from qiskit.aqua.components.uncertainty_models import (LogNormalDistribution,
 from qiskit.finance.components.uncertainty_problems import \
     (EuropeanCallDelta, FixedIncomeExpectedValue)
 from qiskit.aqua.components.uncertainty_problems import \
-                        UnivariatePiecewiseLinearObjective as PwlObjective
+    UnivariatePiecewiseLinearObjective as PwlObjective
 from qiskit.aqua.components.uncertainty_problems import UnivariateProblem
 from qiskit.aqua.algorithms import AmplitudeEstimation, MaximumLikelihoodAmplitudeEstimation
 
 
 class TestEuropeanCallOption(QiskitFinanceTestCase):
     """ Test European Call Option """
+
     def setUp(self):
         super().setUp()
 
@@ -110,7 +111,7 @@ class TestEuropeanCallOption(QiskitFinanceTestCase):
         ['statevector', MaximumLikelihoodAmplitudeEstimation(5),
          {'estimation': 0.16330976193204114}],
         ['qasm', MaximumLikelihoodAmplitudeEstimation(3),
-         {'estimation': 0.1027255930905642}],
+         {'estimation': 0.09808188840384949}],
     ])
     def test_expected_value(self, simulator, a_e, expect):
         """ expected value test """
@@ -151,6 +152,7 @@ class TestEuropeanCallOption(QiskitFinanceTestCase):
 
 class TestFixedIncomeAssets(QiskitFinanceTestCase):
     """ Test Fixed Income Assets """
+
     def setUp(self):
         super().setUp()
 
@@ -170,7 +172,7 @@ class TestFixedIncomeAssets(QiskitFinanceTestCase):
         ['statevector', MaximumLikelihoodAmplitudeEstimation(5),
          {'estimation': 2.340361798381051}],
         ['qasm', MaximumLikelihoodAmplitudeEstimation(5),
-         {'estimation': 2.317921060790118}]
+         {'estimation': 2.3175136132411103}]
     ])
     def test_expected_value(self, simulator, a_e, expect):
         """ expected value test """
