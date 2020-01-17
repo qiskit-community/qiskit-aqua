86c047b7fcd0d47f6a800c541a1cc5e195a6b04e
woodsp <woodsp@us.ibm.com>
2020-01-17 16:28:20 -0500

    Additional unit tests

---
+++ test/optimization/test_tsp.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Test TSP (Travelling Saleman Problem) """
+
+from test.optimization import QiskitOptimizationTestCase
+import numpy as np
+
+from qiskit.aqua import aqua_globals
+from qiskit.optimization.ising import tsp
+from qiskit.optimization.ising.common import sample_most_likely
+from qiskit.aqua.algorithms import ExactEigensolver
+
+
+class TestTSP(QiskitOptimizationTestCase):
+    """TSP Ising tests."""
+
+    def setUp(self):
+        super().setUp()
+        self.seed = 80598
+        aqua_globals.random_seed = self.seed
+        self.num_nodes = 3
+        self.ins = tsp.random_tsp(self.num_nodes)
+        self.qubit_op, self.offset = tsp.get_operator(self.ins)
+        # TestTSP._plot(self.num_nodes, self.ins)  # To visualize test graph if wanted
+
+    # @staticmethod
+    # def _plot(num_nodes, ins):
+    #     import matplotlib.pyplot as plt
+    #     import networkx as nx
+    #
+    #     graph = nx.Graph()
+    #     graph.add_nodes_from(np.arange(0, num_nodes, 1))
+    #     colors = ['r' for node in graph.nodes()]
+    #     pos = {k: v for k, v in enumerate(ins.coord)}
+    #     default_axes = plt.axes(frameon=True)
+    #     nx.draw_networkx(graph, node_color=colors, node_size=600, alpha=.8,
+    #                      ax=default_axes, pos=pos)
+    #     plt.show()
+
+    def test_tsp(self):
+        """ TSP test """
+        algo = ExactEigensolver(self.qubit_op, k=1)
+        result = algo.run()
+        x = sample_most_likely(result['eigvecs'][0])
+        order = tsp.get_tsp_solution(x)
+        np.testing.assert_array_equal(order, [1, 2, 0])
