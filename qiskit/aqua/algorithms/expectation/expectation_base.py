05f0b828ccfa1eca858aaf8df8cb5517022c4e83
Donny Greenberg <dongreenberg2@gmail.com>
2020-02-04 17:37:14 -0500

    More improvements to ExpectationBase factory logic

--- qiskit/aqua/algorithms/expectation/expectation_base.py
+++ qiskit/aqua/algorithms/expectation/expectation_base.py
@@ -18,6 +18,7 @@ import logging
 import numpy as np
 from abc import abstractmethod
 
+from qiskit import BasicAer
 from qiskit.aqua import AquaError, QuantumAlgorithm
 from qiskit.aqua.operators import OpCombo, OpPrimitive
 
@@ -39,23 +40,58 @@ class ExpectationBase():
         """
         primitives = operator.get_primtives()
         if primitives == {'Pauli'}:
-            if backend is not None and is_aer_qasm(backend):
+
+            if backend is None:
+                # If user has Aer but didn't specify a backend, use the Aer fast expectation
+                if has_aer():
+                    from qiskit import Aer
+                    backend = Aer.get_backend('qasm_simulator')
+                # If user doesn't have Aer, use statevector_simulator for < 16 qubits, and qasm with warning for more.
+                else:
+                    if operator.num_qubits <= 16:
+                        backend = BasicAer.get_backend('statevector_simulator')
+                    else:
+                        logging.warning('{0} qubits is a very large expectation value. Consider installing Aer to use '
+                                        'Aer\'s fast expectation, which will perform better here. We\'ll use '
+                                        'the BasicAer qasm backend for this expectation to avoid having to '
+                                        'construct the {1}x{1} operator matrix.'.format(operator.num_qubits,
+                                                                                        2**operator.num_qubits))
+                        backend = BasicAer.get_backend('qasm_simulator')
+
+            # If the user specified Aer qasm backend and is using a Pauli operator, use the Aer fast expectation
+            if is_aer_qasm(backend):
                 from .aer_pauli_expectation import AerPauliExpectation
                 return AerPauliExpectation(operator=operator, backend=backend, state=state)
-            elif backend is None and has_aer():
-                from qiskit import Aer
-                from .aer_pauli_expectation import AerPauliExpectation
-                backend = Aer.get_backend('qasm_simulator')
-                return AerPauliExpectation(operator=operator, backend=backend, state=state)
+
+            # If the user specified a statevector backend (either Aer or BasicAer), use a converter to produce a
+            # Matrix operator and compute using matmul
+            elif is_statevector_backend(backend):
+                from .matrix_expectation import MatrixExpectation
+                backend = BasicAer.get_backend('statevector_simulator')
+                if operator.num_qubits >= 16:
+                    logging.warning('Note: Using a statevector_simulator with {} qubits can be very expensive. '
+                                    'Consider using the Aer qasm_simulator instead to take advantage of Aer\'s '
+                                    'built-in fast Pauli Expectation'.format(operator.num_qubits))
+                # TODO do this properly with converters
+                mat_operator = OpPrimitive(operator.to_matrix())
+                return MatrixExpectation(operator=mat_operator, backend=backend, state=state)
+
+            # All other backends, including IBMQ, BasicAer QASM, go here.
             else:
                 from .pauli_expectation import PauliExpectation
                 return PauliExpectation(operator=operator, backend=backend, state=state)
+
         elif primitives == {'Matrix'}:
             from .matrix_expectation import MatrixExpectation
-            return MatrixExpectation(state=state, operator=operator, backend=backend)
+            return MatrixExpectation(operator=operator, backend=backend, state=state)
+
         elif primitives == {'Instruction'}:
             from .projector_overlap import ProjectorOverlap
-            return ProjectorOverlap(state=state, operator=operator, backend=backend)
+            return ProjectorOverlap(operator=operator, backend=backend, state=state)
+
+        else:
+            # TODO do Pauli to Gate conversion?
+            raise ValueError('Expectations of Mixed Operators not yet supported.')
 
     @abstractmethod
     def compute_expectation(self, state=None):
