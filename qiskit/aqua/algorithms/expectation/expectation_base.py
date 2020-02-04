c1a2da0885def74129bf57f5bedab1cada305920
Donny Greenberg <dongreenberg2@gmail.com>
2020-02-04 17:11:35 -0500

    Add ExpectationBase factory logic

--- qiskit/aqua/algorithms/expectation/expectation_base.py
+++ qiskit/aqua/algorithms/expectation/expectation_base.py
@@ -21,6 +21,10 @@ from abc import abstractmethod
 from qiskit.aqua import AquaError, QuantumAlgorithm
 from qiskit.aqua.operators import OpCombo, OpPrimitive
 
+from qiskit.aqua.utils.backend_utils import (is_statevector_backend,
+                                             is_aer_qasm,
+                                             has_aer)
+
 logger = logging.getLogger(__name__)
 
 
@@ -28,28 +32,35 @@ class ExpectationBase():
     """ A base for Expectation Value algorithms """
 
     @staticmethod
-    def factory(state=None, operator=None, backend=None):
+    def factory(operator, backend=None, state=None):
         """
         Args:
 
         """
-        if isinstance(operator, OpPrimitive):
-            if isinstance(operator.primitive, Pauli):
+        primitives = operator.get_primtives()
+        if primitives == {'Pauli'}:
+            if backend is not None and is_aer_qasm(backend):
+                from .aer_pauli_expectation import AerPauliExpectation
+                return AerPauliExpectation(operator=operator, backend=backend, state=state)
+            elif backend is None and has_aer():
+                from qiskit import Aer
+                from .aer_pauli_expectation import AerPauliExpectation
+                backend = Aer.get_backend('qasm_simulator')
+                return AerPauliExpectation(operator=operator, backend=backend, state=state)
+            else:
                 from .pauli_expectation import PauliExpectation
-                return PauliExpectation(state=state,
-                                        operator=operator,
-                                        backend=backend)
-            elif isinstance(operator.primitive, Instruction):
-                return ProjectorOverlap(state=state,
-                                        operator=operator,
-                                        backend=backend)
-            elif isinstance(operator.primitive, MatrixOperator):
-                return MatmulExpectation(state=state,
-                                        operator=operator,
-                                        backend=backend)
-        elif isinstance(operator, (OpSum, OpVec)):
-            pass
+                return PauliExpectation(operator=operator, backend=backend, state=state)
+        elif primitives == {'Matrix'}:
+            from .matrix_expectation import MatrixExpectation
+            return MatrixExpectation(state=state, operator=operator, backend=backend)
+        elif primitives == {'Instruction'}:
+            from .projector_overlap import ProjectorOverlap
+            return ProjectorOverlap(state=state, operator=operator, backend=backend)
 
     @abstractmethod
-    def compute_expectation(self):
+    def compute_expectation(self, state=None):
+        raise NotImplementedError
+
+    @abstractmethod
+    def compute_variance(self, state=None):
         raise NotImplementedError
