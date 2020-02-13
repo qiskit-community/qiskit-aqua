ec9dbbaff8d41bb69b54123f4dbaf81f98023fc3
Donny Greenberg <dongreenberg2@gmail.com>
2020-02-12 22:09:56 -0500

    Move new tests into 'new' folder for easy separate evaluation.

--- test/aqua/operators/new/test_op_construction.py
+++ test/aqua/operators/new/test_op_construction.py
@@ -12,7 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" Test OpSum """
+""" Test Operator construction, including OpPrimitives and singletons. """
 
 import unittest
 import itertools
