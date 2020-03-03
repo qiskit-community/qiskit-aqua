63592b2d649872eb26232ba0f7f46ed77531594e
woodsp <woodsp@us.ibm.com>
2020-03-02 23:18:29 -0500

    Add minimum eigen solver interface

--- qiskit/aqua/algorithms/minimum_eigen_solvers/classical_minimum_eigen_solver.py
+++ qiskit/aqua/algorithms/minimum_eigen_solvers/classical_minimum_eigen_solver.py
@@ -12,7 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""The Minimum Eigensolver algorithm."""
+"""The Classical Minimum Eigensolver algorithm."""
 
 from typing import List, Optional
 import logging
@@ -20,6 +20,7 @@ import pprint
 import numpy as np
 from qiskit.aqua.algorithms import ClassicalEigensolver
 from qiskit.aqua.operators import BaseOperator
+from .minimum_eigen_solver import MinimumEigensolver
 from .minimum_eigen_solver_result import MinimumEigensolverResult
 
 logger = logging.getLogger(__name__)
@@ -27,23 +28,23 @@ logger = logging.getLogger(__name__)
 
 # pylint: disable=invalid-name
 
-class ClassicalMinimumEigensolver(ClassicalEigensolver):
+class ClassicalMinimumEigensolver(ClassicalEigensolver, MinimumEigensolver):
     """
-    The Minimum Eigensolver algorithm.
+    The Classical Minimum Eigensolver algorithm.
     """
 
-    def __init__(self, operator: BaseOperator,
+    def __init__(self, operator: Optional[BaseOperator] = None,
                  aux_operators: Optional[List[BaseOperator]] = None) -> None:
         """
         Args:
             operator: Operator instance
-            aux_operators: Auxiliary operators to be evaluated at each eigenvalue
+            aux_operators: Auxiliary operators to be evaluated at minimum eigenvalue
         """
         super().__init__(operator, 1, aux_operators)
 
-    def _run(self):
+    def _run(self) -> MinimumEigensolverResult:
         """
-        Run the algorithm to compute up to the requested k number of eigenvalues.
+        Run the algorithm to compute up to the minimum eigenvalue.
         Returns:
             dict: Dictionary of results
         """
@@ -69,3 +70,12 @@ class ClassicalMinimumEigensolver(ClassicalEigensolver):
                      pprint.pformat(result.data, indent=4))
 
         return result
+
+    def compute_minimum_eigenvalue(
+            self, operator: BaseOperator = None,
+            aux_operators: Optional[List[BaseOperator]] = None) -> MinimumEigensolverResult:
+        super().compute_minimum_eigenvalue(operator, aux_operators)
+        return self._run()
+
+    def supports_aux_operators(self) -> bool:
+        return True
