02526477473e4fa1e504b9dcaf93c6e08fbeaa4b
Donny Greenberg <dongreenberg2@gmail.com>
2020-03-05 16:50:18 -0500

    Fix bug in state_fn_operator.py.

--- qiskit/aqua/operators/state_functions/state_fn_operator.py
+++ qiskit/aqua/operators/state_functions/state_fn_operator.py
@@ -168,7 +168,7 @@ class StateFnOperator(StateFn):
             raise ValueError('Cannot compute overlap with StateFn or Operator if not Measurement. Try taking '
                              'sf.adjoint() first to convert to measurement.')
         if isinstance(other, list):
-            return [self.eval(front_elem) for front_elem in front]
+            return [self.eval(front_elem) for front_elem in other]
 
         if not isinstance(other, OperatorBase):
             other = StateFn(other)
