a803b323b3c0d73b477afb72c0dd2579d7f16243
Anton Dekusar <adekusar@ie.ibm.com>
2020-03-25 10:44:54 +0000

    formatting, linting, typing

--- test/optimization/test_admm_miskp.py
+++ test/optimization/test_admm_miskp.py
@@ -1,7 +1,8 @@
-# -*-coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2000.
+# (C) Copyright IBM 2020.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -11,8 +12,10 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Tests of the ADMM algorithm.
-"""
+"""Tests of the ADMM algorithm."""
+from typing import Optional
+
+from test.optimization import QiskitOptimizationTestCase
 
 import numpy as np
 from cplex import SparsePair
@@ -21,21 +24,16 @@ from qiskit.optimization.algorithms import CplexOptimizer, MinimumEigenOptimizer
 from qiskit.optimization.algorithms.admm_optimizer import ADMMOptimizer, ADMMParameters
 from qiskit.optimization.problems import OptimizationProblem
 
-from test.optimization import QiskitOptimizationTestCase
-
 
 class TestADMMOptimizerMiskp(QiskitOptimizationTestCase):
     """ADMM Optimizer Tests based on Mixed-Integer Setup Knapsack Problem"""
 
-    def setUp(self):
-        super().setUp()
-
     def test_admm_optimizer_miskp_eigen(self):
-        """ ADMM Optimizer Test based on Mixed-Integer Setup Knapsack Problem
+        """ADMM Optimizer Test based on Mixed-Integer Setup Knapsack Problem
         using NumPy eigen optimizer"""
-        K, T, P, S, D, C = self.get_problem_params()
-        miskp = Miskp(K, T, P, S, D, C)
+        miskp = Miskp(*self._get_problem_params())
         op: OptimizationProblem = miskp.create_problem()
+        self.assertIsNotNone(op)
 
         # use numpy exact diagonalization
         qubo_optimizer = MinimumEigenOptimizer(NumPyMinimumEigensolver())
@@ -46,11 +44,7 @@ class TestADMMOptimizerMiskp(QiskitOptimizationTestCase):
 
         solver = ADMMOptimizer(params=admm_params)
         solution = solver.solve(op)
-
-        # debug
-        print("results")
-        print("x={}".format(solution.x))
-        print("fval={}".format(solution.fval))
+        self.assertIsNotNone(solution)
 
         correct_solution = [0.009127, 0.009127, 0.009127, 0.009127, 0.009127, 0.009127, 0.009127,
                             0.009127, 0.009127, 0.009127, 0.006151, 0.006151, 0.006151, 0.006151,
@@ -58,70 +52,83 @@ class TestADMMOptimizerMiskp(QiskitOptimizationTestCase):
                             0., 0.]
         correct_objective = -1.2113693
 
+        self.assertIsNotNone(solution.x)
         np.testing.assert_almost_equal(correct_solution, solution.x, 3)
-        np.testing.assert_almost_equal(solution.fval, correct_objective, 3)
+        self.assertIsNotNone(solution.fval)
+        np.testing.assert_almost_equal(correct_objective, solution.fval, 3)
 
-    def get_problem_params(self):
+    @staticmethod
+    def _get_problem_params() -> (int, int, float, np.ndarray, np.ndarray, np.ndarray):
         """Fills in parameters for a Mixed Integer Setup Knapsack Problem (MISKP) instance."""
 
-        K = 2
-        T = 10
-        P = 45.10
-        S = np.asarray([75.61, 75.54])
+        family_count = 2
+        items_per_family = 10
+        knapsack_capacity = 45.10
+        setup_costs = np.asarray([75.61, 75.54])
 
-        D = np.asarray([9.78, 8.81, 9.08, 2.03, 8.9, 9, 4.12, 8.16, 6.55, 4.84, 3.78, 5.13,
-                        4.72, 7.6, 2.83, 1.44, 2.45, 2.24, 6.3, 5.02]).reshape((K, T))
+        resource_values = np.asarray([9.78, 8.81, 9.08, 2.03, 8.9, 9, 4.12, 8.16, 6.55, 4.84, 3.78,
+                                      5.13, 4.72, 7.6, 2.83, 1.44, 2.45, 2.24, 6.3, 5.02]) \
+            .reshape((family_count, items_per_family))
 
-        C = np.asarray([-11.78, -10.81, -11.08, -4.03, -10.90, -11.00, -6.12, -10.16, -8.55, -6.84,
-                        -5.78, -7.13, -6.72, -9.60, -4.83, -3.44, -4.45, -4.24, -8.30, -7.02]) \
-            .reshape((K, T))
+        cost_values = np.asarray([-11.78, -10.81, -11.08, -4.03, -10.90, -11.00, -6.12, -10.16,
+                                  -8.55, -6.84, -5.78, -7.13, -6.72, -9.60, -4.83, -3.44, -4.45,
+                                  -4.24, -8.30, -7.02]) \
+            .reshape((family_count, items_per_family))
 
-        return K, T, P, S, D, C
+        return family_count, items_per_family, knapsack_capacity, setup_costs, \
+               resource_values, cost_values
 
 
 class Miskp:
-    def __init__(self, K: int, T: int, P: float, S: np.ndarray, D: np.ndarray, C: np.ndarray,
-                 pairwise_incomp: int = 0, multiple_choice: int = 0):
-        """
-        Constructor method of the class.
+    """A Helper class to generate  Mixed Integer Setup Knapsack problems"""
+    def __init__(self, family_count: int, items_per_family: int, knapsack_capacity: float,
+                 setup_costs: np.ndarray, resource_values: np.ndarray, cost_values: np.ndarray,
+                 pairwise_incomp: int = 0, multiple_choice: int = 0) -> None:
+        """Constructs an instance of this helper class to create suitable ADMM problems.
 
         Args:
-            K: number of families
-            T: number of items in each family
-            C: value of including item t in family k in the knapsack
-            D: resources consumed if item t in family k is included in the knapsack
-            S: setup cost to include family k in the knapsack
-            P: capacity of the knapsack
+            family_count: number of families
+            items_per_family: number of items in each family
+            knapsack_capacity: capacity of the knapsack
+            setup_costs: setup cost to include family k in the knapsack
+            resource_values: resources consumed if item t in family k is included in the knapsack
+            cost_values: value of including item t in family k in the knapsack
+            pairwise_incomp:
+            multiple_choice:
         """
 
         self.multiple_choice = multiple_choice
         self.pairwise_incomp = pairwise_incomp
-        self.P = P
-        self.S = S
-        self.D = D
-        self.C = C
-        self.T = T
-        self.K = K
+        self.knapsack_capacity = knapsack_capacity
+        self.setup_costs = setup_costs
+        self.resource_values = resource_values
+        self.cost_values = cost_values
+        self.items_per_family = items_per_family
+        self.family_count = family_count
 
         # definitions of the internal variables
         self.op = None
-        self.range_K = None
-        self.range_T = None
+        self.range_family = None
+        self.range_items = None
         self.n_x_vars = None
         self.n_y_vars = None
         self.range_x_vars = None
         self.range_y_vars = None
 
     @staticmethod
-    def var_name(stem, index1, index2=None, index3=None):
-        """A method to return a string representing the name of a decision variable or
+    def _var_name(stem: str, index1: int, index2: Optional[int] = None,
+                  index3: Optional[int] = None) -> str:
+        """A method to return a string representation of the name of a decision variable or
         a constraint, given its indices.
 
-            Args:
-                stem: Element name.
-                index1: Element indices
-                index2: Element indices
-                index3: Element indices
+        Args:
+            stem: Element name.
+            index1: Element indices
+            index2: Element indices
+            index3: Element indices
+
+        Returns:
+            Textual representation of the variable name based on the parameters
         """
         if index2 is None:
             return stem + "(" + str(index1) + ")"
@@ -129,73 +136,79 @@ class Miskp:
             return stem + "(" + str(index1) + "," + str(index2) + ")"
         return stem + "(" + str(index1) + "," + str(index2) + "," + str(index3) + ")"
 
-    def create_params(self):
-        self.range_K = range(self.K)
-        self.range_T = range(self.T)
+    def _create_params(self) -> None:
+        self.range_family = range(self.family_count)
+        self.range_items = range(self.items_per_family)
 
         # make sure instance params are floats
+        self.setup_costs = [float(val) for val in self.setup_costs]
+        self.cost_values = self.cost_values.astype(float)
+        self.resource_values = self.resource_values.astype(float)
 
-        self.S = [float(val) for val in self.S]
-        self.C = self.C.astype(float)
-        self.D = self.D.astype(float)
+        self.n_x_vars = self.family_count * self.items_per_family
+        self.n_y_vars = self.family_count
 
-        self.n_x_vars = self.K * self.T
-        self.n_y_vars = self.K
+        self.range_x_vars = [(k, t) for k in self.range_family for t in self.range_items]
+        self.range_y_vars = self.range_family
 
-        self.range_x_vars = [(k, t) for k in self.range_K for t in self.range_T]
-        self.range_y_vars = self.range_K
-
-    def create_vars(self):
+    def _create_vars(self) -> None:
         self.op.variables.add(
             lb=[0.0] * self.n_x_vars,
-            names=[self.var_name("x", i, j) for i, j in self.range_x_vars])
+            names=[self._var_name("x", i, j) for i, j in self.range_x_vars])
 
         self.op.variables.add(
             # lb=[0.0] * self.n_y_vars,
             # ub=[1.0] * self.n_y_vars,
             types=["B"] * self.n_y_vars,
-            names=[self.var_name("y", i) for i in self.range_y_vars])
+            names=[self._var_name("y", i) for i in self.range_y_vars])
 
-    def create_constraint_capacity(self):
+    def _create_constraint_capacity(self) -> None:
         self.op.linear_constraints.add(
             lin_expr=[
                 SparsePair(
-                    ind=[self.var_name("x", i, j) for i, j in self.range_x_vars]
+                    ind=[self._var_name("x", i, j) for i, j in self.range_x_vars]
                     ,
-                    val=[self.D[i, j] for i, j in self.range_x_vars])
+                    val=[self.resource_values[i, j] for i, j in self.range_x_vars])
             ],
             senses="L",
-            rhs=[self.P],
+            rhs=[self.knapsack_capacity],
             names=["CAPACITY"])
 
-    def create_constraint_allocation(self):
+    def _create_constraint_allocation(self) -> None:
         self.op.linear_constraints.add(
             lin_expr=[
                 SparsePair(
-                    ind=[self.var_name("x", k, t)] + [self.var_name("y", k)],
+                    ind=[self._var_name("x", k, t)] + [self._var_name("y", k)],
                     val=[1.0, -1.0])
                 for k, t in self.range_x_vars
             ],
             senses="L" * self.n_x_vars,
             rhs=[0.0] * self.n_x_vars,
-            names=[self.var_name("ALLOCATION", k, t) for k, t in self.range_x_vars])
+            names=[self._var_name("ALLOCATION", k, t) for k, t in self.range_x_vars])
 
-    def create_constraints(self):
-        self.create_constraint_capacity()
-        self.create_constraint_allocation()
+    def _create_constraints(self) -> None:
+        self._create_constraint_capacity()
+        self._create_constraint_allocation()
 
-    def create_objective(self):
-        self.op.objective.set_linear([(self.var_name("y", k), self.S[k]) for k in self.range_K] +
-                                     [(self.var_name("x", k, t), self.C[k, t]) for k, t in
+    def _create_objective(self) -> None:
+        self.op.objective.set_linear([(self._var_name("y", k), self.setup_costs[k])
+                                      for k in self.range_family] +
+                                     [(self._var_name("x", k, t), self.cost_values[k, t])
+                                      for k, t in
                                       self.range_x_vars]
                                      )
 
-    def create_problem(self):
+    def create_problem(self) -> OptimizationProblem:
+        """Creates an instance of optimization problem based on parameters specified.
+
+        Returns:
+            an instance of optimization problem.
+        """
         self.op = OptimizationProblem()
 
-        self.create_params()
-        self.create_vars()
-        self.create_objective()
-        self.create_constraints()
+        self._create_params()
+        self._create_vars()
+        self._create_objective()
+        self._create_constraints()
 
         return self.op
