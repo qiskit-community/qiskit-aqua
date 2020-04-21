52589f18fc55392f9b69e375972b90e928232ad8
Takashi Imamichi <t.imamichi@gmail.com>
2020-04-21 19:52:39 +0900

    Fix unit tests related to quadratic expressions

--- test/optimization/test_quadratic_objective.py
+++ test/optimization/test_quadratic_objective.py
@@ -14,13 +14,14 @@
 
 """ Test QuadraticObjective """
 
-import unittest
-from test.optimization.optimization_test_case import QiskitOptimizationTestCase
 import logging
+import unittest
+
 import numpy as np
 
 from qiskit.optimization import QuadraticProgram
 from qiskit.optimization.problems.quadratic_objective import ObjSense
+from test.optimization.optimization_test_case import QiskitOptimizationTestCase
 
 logger = logging.getLogger(__name__)
 
@@ -36,34 +37,32 @@ class TestQuadraticObjective(QiskitOptimizationTestCase):
             quadratic_program.continuous_var()
 
         self.assertEqual(quadratic_program.objective.constant, 0.0)
-        self.assertEqual(
-            len(quadratic_program.objective.linear.to_dict()), 0)
-        self.assertEqual(
-            len(quadratic_program.objective.quadratic.to_dict()), 0)
+        self.assertEqual(len(quadratic_program.objective.linear.to_dict()), 0)
+        self.assertEqual(len(quadratic_program.objective.quadratic.to_dict()), 0)
         self.assertEqual(quadratic_program.objective.sense, ObjSense.MINIMIZE)
 
         constant = 1.0
         linear_coeffs = np.array(range(5))
-        quadratic_coeffs = np.array([[i*j for i in range(5)] for j in range(5)])
+        lst = [[0 for _ in range(5)] for _ in range(5)]
+        for i, v in enumerate(lst):
+            for j, _ in enumerate(v):
+                lst[min(i, j)][max(i, j)] += i * j
+        quadratic_coeffs = np.array(lst)
 
         quadratic_program.minimize(constant, linear_coeffs, quadratic_coeffs)
 
         self.assertEqual(quadratic_program.objective.constant, constant)
+        self.assertTrue((quadratic_program.objective.linear.to_array() == linear_coeffs).all())
         self.assertTrue(
-            (quadratic_program.objective.linear.to_array() == linear_coeffs).all())
-        self.assertTrue(
-            (quadratic_program.objective.quadratic.to_array() == quadratic_coeffs)
-            .all())
+            (quadratic_program.objective.quadratic.to_array() == quadratic_coeffs).all())
         self.assertEqual(quadratic_program.objective.sense, ObjSense.MINIMIZE)
 
         quadratic_program.maximize(constant, linear_coeffs, quadratic_coeffs)
 
         self.assertEqual(quadratic_program.objective.constant, constant)
+        self.assertTrue((quadratic_program.objective.linear.to_array() == linear_coeffs).all())
         self.assertTrue(
-            (quadratic_program.objective.linear.to_array() == linear_coeffs).all())
-        self.assertTrue(
-            (quadratic_program.objective.quadratic.to_array() == quadratic_coeffs)
-            .all())
+            (quadratic_program.objective.quadratic.to_array() == quadratic_coeffs).all())
         self.assertEqual(quadratic_program.objective.sense, ObjSense.MAXIMIZE)
 
         self.assertEqual(quadratic_program.objective.evaluate(linear_coeffs), 931.0)
@@ -77,18 +76,20 @@ class TestQuadraticObjective(QiskitOptimizationTestCase):
 
         constant = 1.0
         linear_coeffs = np.array(range(5))
-        quadratic_coeffs = np.array([[i*j for i in range(5)] for j in range(5)])
+        lst = [[0 for _ in range(5)] for _ in range(5)]
+        for i, v in enumerate(lst):
+            for j, _ in enumerate(v):
+                lst[min(i, j)][max(i, j)] += i * j
+        quadratic_coeffs = np.array(lst)
 
         quadratic_program.objective.constant = constant
         quadratic_program.objective.linear = linear_coeffs
         quadratic_program.objective.quadratic = quadratic_coeffs
 
         self.assertEqual(quadratic_program.objective.constant, constant)
+        self.assertTrue((quadratic_program.objective.linear.to_array() == linear_coeffs).all())
         self.assertTrue(
-            (quadratic_program.objective.linear.to_array() == linear_coeffs).all())
-        self.assertTrue(
-            (quadratic_program.objective.quadratic.to_array() == quadratic_coeffs)
-            .all())
+            (quadratic_program.objective.quadratic.to_array() == quadratic_coeffs).all())
 
         self.assertEqual(quadratic_program.objective.sense, ObjSense.MINIMIZE)
 
