9fc0564c37dece4cbf2d5ff8bea1b35078a1135c
Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>
2020-05-02 03:51:16 +0200

    Update doc styles and fix refs. (#949)
    * Update doc styles and fix refs.

--- docs/conf.py
+++ docs/conf.py
@@ -72,9 +72,7 @@ extensions = [
 ]
 html_static_path = ['_static']
 templates_path = ['_templates']
-html_css_files = [
-    'style.css',
-]
+html_css_files = ['style.css', 'custom.css']
 
 # -----------------------------------------------------------------------------
 # Autosummary
@@ -89,7 +87,6 @@ autosummary_generate_overwrite = False
 
 autodoc_default_options = {
     'inherited-members': None,
-    'show-inheritance': None,
 }
 
 
@@ -113,7 +110,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'colorful'
