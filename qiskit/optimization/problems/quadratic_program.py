2d525c385c2e8b456976a1ce91d8355b4e3f5dd8
Cryoris <jules.gacon@googlemail.com>
2020-05-07 10:46:50 +0200

    make optimizations stack Py 3.5 compatible

--- qiskit/optimization/problems/quadratic_program.py
+++ qiskit/optimization/problems/quadratic_program.py
@@ -64,14 +64,14 @@ class QuadraticProgram:
         self._name = name
         self._status = QuadraticProgram.Status.VALID
 
-        self._variables: List[Variable] = []
-        self._variables_index: Dict[str, int] = {}
+        self._variables = []  # List[Variable]
+        self._variables_index = {}  # Dict[str, int]
 
-        self._linear_constraints: List[LinearConstraint] = []
-        self._linear_constraints_index: Dict[str, int] = {}
+        self._linear_constraints = []  # List[LinearConstraint]
+        self._linear_constraints_index = {}  # Dict[str, int]
 
-        self._quadratic_constraints: List[QuadraticConstraint] = []
-        self._quadratic_constraints_index: Dict[str, int] = {}
+        self._quadratic_constraints = []  # List[QuadraticConstraint]
+        self._quadratic_constraints_index = {}  # Dict[str, int]
 
         self._objective = QuadraticObjective(self)
 
@@ -868,8 +868,8 @@ class SubstituteVariables:
     CONST = '__CONSTANT__'
 
     def __init__(self):
-        self._src: Optional[QuadraticProgram] = None
-        self._dst: Optional[QuadraticProgram] = None
+        self._src = None  # Optional[QuadraticProgram]
+        self._dst = None  # Optional[QuadraticProgram]
         self._subs = {}
 
     def substitute_variables(
@@ -943,7 +943,7 @@ class SubstituteVariables:
 
     def _subs_dict(self, constants, variables):
         # guarantee that there is no overlap between variables to be replaced and combine input
-        subs: Dict[str, Tuple[str, float]] = {}
+        subs = {}  # Dict[str, Tuple[str, float]]
         if constants is not None:
             for i, v in constants.items():
                 # substitute i <- v
