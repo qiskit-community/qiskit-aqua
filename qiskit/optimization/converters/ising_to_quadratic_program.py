f38589f27fdedc724b2112fdbd1e46e6c4e3c33c
Atsushi Matsuo <MATSUOA@jp.ibm.com>
2020-05-08 18:23:58 +0900

    fixed minor lint issues

--- qiskit/optimization/converters/ising_to_quadratic_program.py
+++ qiskit/optimization/converters/ising_to_quadratic_program.py
@@ -30,7 +30,8 @@ class IsingToQuadraticProgram:
     def __init__(self, linear: bool = False) -> None:
         """Initialize the internal data structure.
         Args:
-            linear: If linear is True, x^2 is treated as a linear term since x^2 = x for x \in {0,1}.
+            linear: If linear is True, x^2 is treated as a linear term
+                    since x^2 = x for x in {0,1}.
                     Else, x^2 is treat as a quadratic term
         """
         self._qubit_op = None
