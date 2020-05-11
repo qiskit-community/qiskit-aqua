e1d32efc8059efca0ff3d0d6eec8ed889e9de650
Donny Greenberg <dongreenberg2@gmail.com>
2020-05-11 19:38:15 -0400

    Update docs

--- qiskit/aqua/operators/operator_base.py
+++ qiskit/aqua/operators/operator_base.py
@@ -405,14 +405,17 @@ class OperatorBase(ABC):
 
     @abstractmethod
     def assign_parameters(self,
-                          param_dict: Dict[ParameterExpression, Union[Number, List[Number]]]
+                          param_dict: Dict[ParameterExpression,
+                                           Union[Number, ParameterExpression,
+                                                 List[Number, ParameterExpression]]]
                           ) -> 'OperatorBase':
         """ Binds scalar values to any Terra ``Parameters`` in the coefficients or primitives of
-        the Operator. This method differs from Terra's ``bind_parameters`` in that it also
-        supports lists of scalar values to bind for a give ``Parameter``, in which case self will be
-        copied for each parameterization in the binding list(s), and all the copies will be
-        returned in an ``OpList``. If lists of parameterizations used, every ``Parameter`` in the
-        param_dict must have the same length list of parameterizations.
+        the Operator, or substitutes one ``Parameter`` for another. This method differs from
+        Terra's ``assign_parameters`` in that it also supports lists of values to assign for a
+        give ``Parameter``, in which case self will be copied for each parameterization in the
+        binding list(s), and all the copies will be returned in an ``OpList``. If lists of
+        parameterizations are used, every ``Parameter`` in the param_dict must have the same
+        length list of parameterizations.
 
         Args:
             param_dict: The dictionary of ``Parameters`` to replace, and values or lists of
@@ -420,13 +423,15 @@ class OperatorBase(ABC):
 
         Returns:
             The ``OperatorBase`` with the ``Parameters`` in self replaced by the
-            value bindings in param_dict. If param_dict contains parameterization lists,
+            values or ``Parameters`` in param_dict. If param_dict contains parameterization lists,
             this ``OperatorBase`` is an ``OpList``.
         """
         raise NotImplementedError
 
     def bind_parameters(self,
-                        param_dict: Dict[ParameterExpression, Union[Number, List[Number]]]
+                        param_dict: Dict[ParameterExpression,
+                                         Union[Number, ParameterExpression,
+                                               List[Number, ParameterExpression]]]
                         ) -> 'OperatorBase':
         r"""
         Same as assign_parameters, but maintained for consistency with QuantumCircuit in
