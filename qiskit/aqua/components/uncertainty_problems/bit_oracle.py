8aaf6b79d01ae1992948bd661f6c55ec913e6102
Cryoris <jules.gacon@googlemail.com>
2020-05-28 16:45:50 +0200

    add grover op and bit oracle

---
+++ qiskit/aqua/components/uncertainty_problems/bit_oracle.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""The Bit oracle."""
+
+from typing import List
+from qiskit.circuit import QuantumCircuit
+from qiskit.circuit.library import MCXGate
+
+
+class BitOracle(QuantumCircuit):
+    """The bit oracle.
+
+    Adds a -1 phase if all objective qubits are in state 1.
+    """
+
+    def __init__(self, num_state_qubits: int,
+                 objective_qubits: List[int],
+                 mcx_mode: str = 'noancilla',
+                 name: str = 'S_Psi0') -> None:
+        """
+        Args:
+            num_state_qubits: The number of qubits.
+            objective_qubits: The objective qubits.
+            mcx_mode: The mode for the multi-controlled X gate.
+            name: The name of the circuit.
+
+        Raises:
+            ValueError: If ``objective_qubits`` contains an invalid index.
+        """
+        self._num_ancilla_qubits = MCXGate.get_num_ancilla_qubits(len(objective_qubits) - 1,
+                                                                  mcx_mode)
+        super().__init__(num_state_qubits + self._num_ancilla_qubits, name=name)
+
+        if any(qubit >= num_state_qubits for qubit in objective_qubits):
+            raise ValueError('Qubit index out of range, max {}, provided {}'.format(
+                num_state_qubits, objective_qubits
+                )
+            )
+
+        ancilla_qubits = list(range(num_state_qubits, num_state_qubits + self._num_ancilla_qubits))
+
+        self.h(objective_qubits)
+        self.mcx(objective_qubits[:-1], objective_qubits[-1],
+                 ancilla_qubits, mode=mcx_mode)
+        self.h(objective_qubits)
+
+    @property
+    def num_ancilla_qubits(self) -> int:
+        """The number of ancilla qubits."""
+        return self._num_ancilla_qubits
---
+++ qiskit/aqua/components/uncertainty_problems/grover_operator.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""The Grover operator."""
+
+from typing import List, Optional
+from qiskit.circuit import QuantumCircuit, QuantumRegister
+
+
+class GroverOperator(QuantumCircuit):
+    """The Grover operator."""
+
+    def __init__(self, oracle: QuantumCircuit,
+                 a_operator: Optional[QuantumCircuit] = None,
+                 zero_reflection: Optional[QuantumCircuit] = None,
+                 idle_qubits: Optional[List[int]] = None,
+                 name: str = 'Q') -> None:
+        """
+        Args:
+            oracle: The oracle implementing a reflection about the bad state.
+            a_operator: The operator preparing the good and bad state. For Grover's algorithm,
+                this is a n-qubit Hadamard gate and for Amplitude Amplification or Estimation
+                the operator A.
+            zero_reflection: The reflection about the zero state.
+            idle_qubits: Qubits that are ignored in the reflection about zero.
+            name: The name of the circuit.
+        """
+        super().__init__(name=name)
+        self._oracle = oracle
+        self._a_operator = a_operator
+        self._zero_reflection = zero_reflection
+        self._idle_qubits = idle_qubits
+
+        self._build()
+
+    @property
+    def num_state_qubits(self):
+        """The number of state qubits."""
+        if hasattr(self._oracle, 'num_state_qubits'):
+            return self._oracle.num_state_qubits
+        return self._oracle.num_qubits
+
+    @property
+    def num_ancilla_qubits(self) -> int:
+        """The number of ancilla qubits.
+
+        Returns:
+            The number of ancilla qubits in the circuit.
+        """
+        max_num_ancillas = 0
+        if self._zero_reflection:
+            max_num_ancillas = self._zero_reflection.num_ancilla_qubits
+        elif self._oracle.num_qubits > 1:
+            max_num_ancillas = 1
+
+        if self._a_operator and hasattr(self._a_operator, 'num_ancilla_qubits'):
+            max_num_ancillas = max(max_num_ancillas, self._a_operator.num_ancilla_qubits)
+
+        if hasattr(self._a_operator, 'num_ancilla_qubits'):
+            max_num_ancillas = max(max_num_ancillas, self._oracle.num_ancilla_qubits)
+
+        return max_num_ancillas
+
+    @property
+    def num_qubits(self):
+        """The number of qubits in the Grover operator."""
+        return self.num_state_qubits + self.num_ancilla_qubits
+
+    @property
+    def zero_reflection(self) -> QuantumCircuit:
+        """The subcircuit implementing the reflection about 0."""
+        if self._zero_reflection is not None:
+            return self._zero_reflection
+
+        zero_reflection = QuantumCircuit(self.num_qubits, name='S_0')
+        if self.num_qubits == 1:
+            zero_reflection.z(0)
+        else:
+            zero_reflection.x(list(range(self.num_qubits)))
+            zero_reflection.h(self.num_qubits - 1)
+            zero_reflection.mcx(list(range(self.num_qubits - 1)), self.num_qubits - 1)
+            zero_reflection.h(self.num_qubits - 1)
+            zero_reflection.x(list(range(self.num_qubits)))
+
+        return zero_reflection
+
+    @property
+    def a_operator(self) -> QuantumCircuit:
+        """The subcircuit implementing the A operator or Hadamards."""
+        if self._a_operator:
+            return self._a_operator
+
+        hadamards = QuantumCircuit(self.num_state_qubits, name='H')
+        hadamards.h(list(range(self.num_state_qubits)))
+        return hadamards
+
+    @property
+    def oracle(self):
+        """The oracle implementing a relfection about the bad state."""
+        return self._oracle
+
+    def _build(self):
+        qr_state = QuantumRegister(self.num_state_qubits, name='state')
+        qr_ancilla = QuantumRegister(self.num_ancilla_qubits, name='ancilla')
+        self.qregs = [qr_state, qr_ancilla]
+
+        _append(self, self.a_operator)
+        _append(self, self.zero_reflection)
+        _append(self, self.a_operator.inverse())
+        _append(self, self.oracle)
+
+
+def _append(target, other, qubits=None):
+    if hasattr(other, 'num_state_qubits') and hasattr(other, 'num_ancilla_qubits'):
+        num_state_qubits = other.num_state_qubits
+        num_ancilla_qubits = other.num_ancilla_qubits
+    else:
+        num_state_qubits = other.num_qubits
+        num_ancilla_qubits = 0
+
+    if qubits is None:
+        qubits = list(range(num_state_qubits + num_ancilla_qubits))
+
+    target.append(other.to_instruction(), qubits)
