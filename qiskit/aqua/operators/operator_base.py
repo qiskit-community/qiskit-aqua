b0062141e078c4a15147f51bd201b41c501b4a8d
molar-volume <mirotomasik@gmail.com>
2020-08-04 17:48:59 +0200

    unexpected indentation fixed.

--- qiskit/aqua/operators/operator_base.py
+++ qiskit/aqua/operators/operator_base.py
@@ -444,7 +444,7 @@ class OperatorBase(ABC):
 
     @abstractmethod
     def expand_to_dim(self, num_qubits: int) -> 'OperatorBase':
-        """ Expand the operator with identity operator of dimension 2**num_qubits.
+        r""" Expands the operator with identity operator of dimension 2**num_qubits.
         Returns:
             Operator corresponding to self.tensor(identity_operator), where dimension of identity
             operator is 2 ** num_qubits.
