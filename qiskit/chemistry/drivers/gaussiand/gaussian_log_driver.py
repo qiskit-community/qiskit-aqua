72916110671bdbe6a9d7087fb39169179f0aa9bf
woodsp <woodsp@us.ibm.com>
2020-08-08 23:02:26 -0400

    Add G16 driver giving log file result

---
+++ qiskit/chemistry/drivers/gaussiand/gaussian_log_driver.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Gaussian Log Driver """
+
+from typing import Union, List
+import logging
+from subprocess import Popen, PIPE
+from shutil import which
+
+from qiskit.chemistry import QiskitChemistryError
+from .gaussiandriver import GAUSSIAN_16, GAUSSIAN_16_DESC
+from .gaussian_log_result import GaussianLogResult
+
+logger = logging.getLogger(__name__)
+
+G16PROG = which(GAUSSIAN_16)
+
+
+class GaussianLogDriver:
+    """
+    Qiskit chemistry driver using the Gaussian™ 16 program that provides the log
+    back, via :class:`GaussianLogResult`, for access to the log and data recorded there.
+
+    See http://gaussian.com/gaussian16/
+
+    This driver does not use Gaussian 16 interfacing code, as certain data such as forces
+    properties are not present in the MatrixElement file. The log is returned as a
+    class:`GaussianLogResult` allowing it to be parsed for whatever data may be of interest.
+    This result class also contains ready access to certain data within the log.
+    """
+
+    def __init__(self, config: Union[str, List[str]]) -> None:
+        r"""
+        Args:
+            config: A molecular configuration conforming to Gaussian™ 16 format. This can
+                be provided as a string string with '\n' line separators or as a list of
+                strings.
+        Raises:
+            QiskitChemistryError: Invalid Input
+        """
+        GaussianLogDriver._check_valid()
+
+        if not isinstance(config, list) and not isinstance(config, str):
+            raise QiskitChemistryError("Invalid input for Gaussian Log Driver '{}'"
+                                       .format(config))
+
+        if isinstance(config, list):
+            config = '\n'.join(config)
+
+        self._config = config
+
+    @staticmethod
+    def _check_valid():
+        if G16PROG is None:
+            raise QiskitChemistryError(
+                "Could not locate {} executable '{}'. Please check that it is installed correctly."
+                .format(GAUSSIAN_16_DESC, GAUSSIAN_16))
+
+    def run(self) -> GaussianLogResult:
+        # The config, i.e. job control file, needs to end with a blank line to be valid for
+        # Gaussian to process it. We simply add the blank line here if not.
+        cfg = self._config
+        while not cfg.endswith('\n\n'):
+            cfg += '\n'
+
+        logger.debug("User supplied configuration raw: '%s'",
+                     cfg.replace('\r', '\\r').replace('\n', '\\n'))
+        logger.debug('User supplied configuration\n%s', cfg)
+
+        return GaussianLogDriver._run_g16(cfg)
+
+    @staticmethod
+    def _run_g16(cfg: str) -> GaussianLogResult:
+
+        # Run Gaussian 16. We capture stdout and if error log the last 10 lines that
+        # should include the error description from Gaussian
+        process = None
+        try:
+            process = Popen(GAUSSIAN_16, stdin=PIPE, stdout=PIPE, universal_newlines=True)
+            stdout, _ = process.communicate(cfg)
+            process.wait()
+        except Exception:
+            if process is not None:
+                process.kill()
+
+            raise QiskitChemistryError('{} run has failed'.format(GAUSSIAN_16_DESC))
+
+        if process.returncode != 0:
+            errmsg = ""
+            if stdout is not None:
+                lines = stdout.splitlines()
+                start = 0
+                if len(lines) > 10:
+                    start = len(lines) - 10
+                for i in range(start, len(lines)):
+                    logger.error(lines[i])
+                    errmsg += lines[i] + "\n"
+            raise QiskitChemistryError(
+                '{} process return code {}\n{}'.format(
+                    GAUSSIAN_16_DESC, process.returncode, errmsg))
+
+        alltext = ""
+        if stdout is not None:
+            lines = stdout.splitlines()
+            for line in lines:
+                alltext += line + "\n"
+
+        if not alltext:
+            raise QiskitChemistryError("Failed to capture log from stdout")
+
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug("Gaussian output:\n%s", alltext)
+
+        return GaussianLogResult(alltext)
+
---
+++ qiskit/chemistry/drivers/gaussiand/gaussian_log_result.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Gaussian Log File Parser """
+
+from typing import Union, List, Tuple
+import copy
+import logging
+import re
+
+logger = logging.getLogger(__name__)
+
+
+class GaussianLogResult:
+    """ Result for Gaussian™ 16 log driver.
+
+    This result allows access to selected data from the log file that is not available
+    via the use Gaussian 16 interfacing code when using the MatrixElement file.
+    Since this parses the text output it is subject to the format of the log file.
+    """
+    def __init__(self, log: Union[str, List[str]]) -> None:
+        """
+        Args:
+            log: The log contents conforming to Gaussian™ 16 format either as a single string
+                 containing new line characters, or as a list of strings. If the single string
+                 has no new line characters it is treated a file name and will be read (a valid
+                 log file contents is multiple lines).
+        Raises:
+            ValueError: Invalid Input
+        """
+
+        self._log = None
+
+        if isinstance(log, str):
+            lines = log.split('\n')
+
+            if len(lines) == 1:
+                with open(lines[0]) as file:
+                    self._log = file.read().split('\n')
+            else:
+                self._log = lines
+
+        elif isinstance(log, list):
+            self._log = log
+
+        else:
+            raise ValueError("Invalid input for Gaussian Log Parser '{}'".format(log))
+
+    @property
+    def log(self) -> List[str]:
+        """ Gets the complete log in the form of a list of strings """
+        return copy.copy(self._log)
+
+    def __str__(self):
+        return '\n'.join(self._log)
+
+    # Sections of interest in the log file
+    SECTION_QUADRATIC = r':\s+QUADRATIC\sFORCE\sCONSTANTS\sIN\sNORMAL\sMODES'
+    SECTION_CUBIC = r':\s+CUBIC\sFORCE\sCONSTANTS\sIN\sNORMAL\sMODES'
+    SECTION_QUARTIC = r':\s+QUARTIC\sFORCE\sCONSTANTS\sIN\sNORMAL\sMODES'
+
+    PATTERN_QUADRATIC_CONST = r'''
+    \s+(?P<index1>\d+)
+    \s+(?P<index2>\d+)
+    \s+(?P<const1>[+-]?\d+\.\d+)
+    \s+(?P<const2>[+-]?\d+\.\d+)
+    \s+(?P<const3>[+-]?\d+\.\d+)
+    '''
+
+    @property
+    def quadratic_force_constants(self) -> List[Tuple[str, str, float, float, float]]:
+        """ Quadratic force constants: 2 indices and 3 constant values """
+        return self._force_constants(self.SECTION_QUADRATIC, 2)
+
+    @property
+    def cubic_force_constants(self) -> List[Tuple[str, str, str, float, float, float]]:
+        """ Cubic force constants: 3 indices and 3 constant values """
+        return self._force_constants(self.SECTION_QUADRATIC, 3)
+
+    @property
+    def quartic_force_constants(self) -> List[Tuple[str, str, str, str, float, float, float]]:
+        """ Cubic force constants: 4 indices and 3 constant values """
+        return self._force_constants(self.SECTION_QUADRATIC, 4)
+
+    def _force_constants(self, section_name: str, indices: int) -> List[Tuple]:
+        constants = []
+        pattern_constants = ''
+        for i in range(indices):
+            pattern_constants += r'\s+(?P<index{}>\w+)'.format(i+1)
+        for i in range(3):
+            pattern_constants += r'\s+(?P<const{}>[+-]?\d+\.\d+)'.format(i+1)
+
+        # Find the section of interest
+        i = 0
+        found_section = False
+        for i, line in enumerate(self._log):
+            if re.search(section_name, line) is not None:
+                found_section = True
+                break
+
+        # Now if section found look from this point on to get the corresponding constant data lines
+        # which is from when we start to get a match against the constants pattern until we
+        # do not again.
+        const_found = False
+        if found_section:
+            for j, line in enumerate(self._log[i:]):
+                if not const_found:
+                    # If we have not found the first line that matches we keep looking
+                    # until we get a match (non-None) and then drop through into found
+                    # section which we use thereafter
+                    const = re.match(pattern_constants, line)
+                    const_found = const is not None
+
+                if const_found:
+                    # If we found the match then for each line we want the contents until
+                    # such point as it does not match anymore then we break out
+                    const = re.match(pattern_constants, line)
+                    if const is not None:
+                        clist = []
+                        for i in range(indices):
+                            clist.append(const.group('index{}'.format(i + 1)))
+                        for i in range(3):
+                            clist.append(float(const.group('const{}'.format(i + 1))))
+                        constants.append(tuple(clist))
+                    else:
+                        break   # End of matching lines
+
+        return constants
+
+
+
