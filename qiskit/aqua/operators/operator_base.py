ee039134f7b0991656681cf307bb6317cbae27b0
Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>
2020-08-22 11:56:38 -0400

    Update qiskit/aqua/operators/operator_base.py

--- qiskit/aqua/operators/operator_base.py
+++ qiskit/aqua/operators/operator_base.py
@@ -86,8 +86,8 @@ class OperatorBase(ABC):
         Returns:
             The output of the Operator's evaluation function. If self is a ``StateFn``, the result
             is a float or complex. If self is an Operator (``PrimitiveOp, ComposedOp, SummedOp,
-            EvolvedOp,`` etc.), the result is a StateFn. 
-            If ``front`` is None, the matrix-representation of the operator is returned, which 
+            EvolvedOp,`` etc.), the result is a StateFn.
+            If ``front`` is None, the matrix-representation of the operator is returned, which
             is a ``MatrixOp`` for the operators and a ``VectorStateFn`` for state-functions.
             If either self or front contain proper
             ``ListOps`` (not ListOp subclasses), the result is an n-dimensional list of complex
