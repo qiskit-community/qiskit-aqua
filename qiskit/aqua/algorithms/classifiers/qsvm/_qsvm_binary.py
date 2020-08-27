bfcd34b7a0d8de12499d129203ffa2ae9aa4c8f0
Stefan Woerner <WOR@zurich.ibm.com>
2020-08-27 22:04:07 +0200

    Update _qsvm_binary.py

--- qiskit/aqua/algorithms/classifiers/qsvm/_qsvm_binary.py
+++ qiskit/aqua/algorithms/classifiers/qsvm/_qsvm_binary.py
@@ -78,7 +78,6 @@ class _QSVM_Binary(_QSVM_ABC):
         """
         scaling = 1.0 if self._qalgo.quantum_instance.is_statevector else None
         kernel_matrix = self._qalgo.construct_kernel_matrix(data)
-
         labels = labels * 2 - 1  # map label from 0 --> -1 and 1 --> 1
         labels = labels.astype(np.float)
         [alpha, b, support] = optimize_svm(kernel_matrix, labels, scaling=scaling)
