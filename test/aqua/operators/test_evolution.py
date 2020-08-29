4ed804ebc95828b194a9bbcc0dd455c66d2f8dc3
John Lapeyre <jlapeyre@users.noreply.github.com>
2020-08-28 22:16:16 -0700

    Make CircuitOp use Operator rather than unitary simulator to get matrix
    The unitary simulator does not account for the global phase in a
    circuit. qiskit.quantum_info.Operator does account for global phase
    when converting (when possible) a circuit to a unitary matrix.
    
    Closes #1218

--- test/aqua/operators/test_evolution.py
+++ test/aqua/operators/test_evolution.py
@@ -20,6 +20,7 @@ from test.aqua import QiskitAquaTestCase
 import numpy as np
 import scipy.linalg
 
+import qiskit
 from qiskit.circuit import ParameterVector, Parameter
 
 from qiskit.aqua.operators import (X, Y, Z, I, CX, H, ListOp, CircuitOp, Zero, EvolutionFactory,
@@ -30,6 +31,13 @@ from qiskit.aqua.operators import (X, Y, Z, I, CX, H, ListOp, CircuitOp, Zero, E
 
 class TestEvolution(QiskitAquaTestCase):
     """Evolution tests."""
+
+    def test_exp_i(self):
+        """ exponential of Pauli test """
+        op = Z.exp_i()
+        gate = op.to_circuit().data[0][0]
+        self.assertIsInstance(gate, qiskit.circuit.library.RZGate)
+        self.assertEqual(gate.params[0], 2)
 
     def test_pauli_evolution(self):
         """ pauli evolution test """
