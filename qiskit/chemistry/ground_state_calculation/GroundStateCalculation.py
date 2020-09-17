199c97b69b90bfb81007e1d2f04aa8de5a7fe595
Max Rossmannek <oss@zurich.ibm.com>
2020-09-17 16:25:45 +0200

    Implement the AdaptVQEGroundStateCalculation class
    This properly implements the AdaptVQEGroundStateCalculation class.
    The idea of this class is to allow easy use of the VQEAdapt algorithm
    which was previously extended to support the MinimumEigensolver
    interface. This Calculation class is solely for the use with this
    algorithm which is why it does not support setting a different solver.
    Instead, it requires setting a QuantumInstance object through its
    initializer.
    
    The changes which this requires include the following:
    
    - fixing the GroundStateCalculation class
    - fixing the MinEigensolverGroundStateCalculation class
    
    Furthermore, a very basic unittest for this change was added.

--- qiskit/chemistry/ground_state_calculation/GroundStateCalculation.py
+++ qiskit/chemistry/ground_state_calculation/GroundStateCalculation.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # This code is part of Qiskit.
 #
 # (C) Copyright IBM 2020.
@@ -12,14 +10,23 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+"""
+A ground state calculation interface.
+"""
 
 from abc import ABC, abstractmethod
-from qiskit.chemistry.core import (Hamiltonian, TransformationType, QubitMappingType,
-                                   ChemistryOperator, MolecularGroundStateResult)
+from typing import List, Optional, Union, Tuple, Dict, Callable
+
+from qiskit.aqua.operators import LegacyBaseOperator
+from qiskit.aqua.algorithms import MinimumEigensolver
 from qiskit.aqua.operators import Z2Symmetries
+from qiskit.chemistry.drivers import BaseDriver
+from qiskit.chemistry.core import (Hamiltonian, TransformationType, QubitMappingType,
+                                   MolecularGroundStateResult)
+
 
 class GroundStateCalculation(ABC):
-    """GroundStateCalculation"""
+    """The ground state calculation interface."""
 
     def __init__(self,
                  transformation: TransformationType = TransformationType.FULL,
@@ -28,24 +35,23 @@ class GroundStateCalculation(ABC):
                  freeze_core: bool = False,
                  orbital_reduction: Optional[List[int]] = None,
                  z2symmetry_reduction: Optional[Union[str, List[int]]] = None)->None:
-       """
-
-       Args:
-           transformation: full or particle_hole
-           qubit_mapping: jordan_wigner, parity or bravyi_kitaev
-           two_qubit_reduction: Whether two qubit reduction should be used,
-                                 when parity mapping only
-           freeze_core: Whether to freeze core orbitals when possible
-           orbital_reduction: Orbital list to be frozen or removed
-           z2symmetry_reduction: If z2 symmetry reduction should be applied to the qubit operators
-                that are computed. Setting 'auto' will
-                use an automatic computation of the correct sector. If from other experiments, with
-                the z2symmetry logic, the sector is known, then the tapering values of that sector
-                can be provided (a list of int of values -1, and 1). The default is None
-                meaning no symmetry reduction is done.
-                See also :class:`~qiskit.chemistry.core.Hamiltonian` which has the core
-                processing behind this class.
-       """
+        """
+        Args:
+            transformation: full or particle_hole
+            qubit_mapping: jordan_wigner, parity or bravyi_kitaev
+            two_qubit_reduction: Whether two qubit reduction should be used,
+                                  when parity mapping only
+            freeze_core: Whether to freeze core orbitals when possible
+            orbital_reduction: Orbital list to be frozen or removed
+            z2symmetry_reduction: If z2 symmetry reduction should be applied to the qubit operators
+                 that are computed. Setting 'auto' will
+                 use an automatic computation of the correct sector. If from other experiments, with
+                 the z2symmetry logic, the sector is known, then the tapering values of that sector
+                 can be provided (a list of int of values -1, and 1). The default is None
+                 meaning no symmetry reduction is done.
+                 See also :class:`~qiskit.chemistry.core.Hamiltonian` which has the core
+                 processing behind this class.
+        """
         self._transformation = transformation
         self._qubit_mapping = qubit_mapping
         self._two_qubit_reduction = two_qubit_reduction
@@ -53,39 +59,60 @@ class GroundStateCalculation(ABC):
         self._orbital_reduction = orbital_reduction
         self._z2symmetry_reduction = z2symmetry_reduction
 
-    def _transform(self, driver):
-        """
+        # this is to provide access to the internal Hamiltonian object in derived classes
+        self._core = None
+
+    def _transform(self, driver: BaseDriver) -> Tuple[LegacyBaseOperator, List]:
+        """Constructs a qubit operator for a given chemical problem.
 
         Args:
-            driver:
+            driver: A chemical driver instance encoding the molecular problem.
 
         Returns:
-
+            The qubit operator and auxiliary operator list transformed based on the specified
+            mapping.
         """
-        # takes driver, applies specified mapping, returns qubit operator
+        q_molecule = driver.run()
 
-        q_molecule = self.driver.run()
+        self._core = Hamiltonian(transformation=self._transformation,
+                                 qubit_mapping=self._qubit_mapping,
+                                 two_qubit_reduction=self._two_qubit_reduction,
+                                 freeze_core=self._freeze_core,
+                                 orbital_reduction=self._orbital_reduction,
+                                 z2symmetry_reduction=self._z2symmetry_reduction)
 
-        core = Hamiltonian(transformation=self._transformation,
-                           qubit_mapping=self._qubit_mapping,
-                           two_qubit_reduction=self._two_qubit_reduction,
-                           freeze_core=self._freeze_core,
-                           orbital_reduction=self._orbital_reduction,
-                           z2symmetry_reduction=self._z2symmetry_reduction)
-
-        self._core = core
-        
-        operator, aux_operators = core.run(q_molecule)
+        operator, aux_operators = self._core.run(q_molecule)
 
         return operator, aux_operators
 
     @property
-    def molecule_info(self):
-
+    def molecule_info(self) -> Dict:
+        """Returns the molecular info stored in the core Hamiltonian."""
         return self._core.molecule_info
-    
+
     @abstractmethod
     def compute_ground_state(self,
-                             driver: BaseDriver) -> MolecularGroundStateResult:
+                             driver: BaseDriver,
+                             callback: Optional[Callable[[List, int, str, bool, Z2Symmetries],
+                                                         MinimumEigensolver]] = None
+                             ) -> MolecularGroundStateResult:
+        """
+        Compute the ground state energy of the molecule that was supplied via the driver.
+
+        Args:
+            driver: a chemical driver
+            callback: If not None will be called with the following values
+                num_particles, num_orbitals, qubit_mapping, two_qubit_reduction, z2_symmetries
+                in that order. This information can then be used to setup chemistry
+                specific component(s) that are needed by the chosen MinimumEigensolver.
+                The MinimumEigensolver can then be built and returned from this callback
+                for use as the solver here.
+
+        Returns:
+            A molecular ground state result
+        Raises:
+            QiskitChemistryError: If no MinimumEigensolver was given and no callback is being
+                                  used that could supply one instead.
+        """
 
         raise NotImplementedError()
--- qiskit/chemistry/ground_state_calculation/MinEigensolverGroundStateCalculation.py
+++ qiskit/chemistry/ground_state_calculation/MinEigensolverGroundStateCalculation.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # This code is part of Qiskit.
 #
 # (C) Copyright IBM 2020.
@@ -14,9 +12,19 @@
 
 """ Ground state computation using Aqua minimum eigensolver """
 
-from qiskit.chemistry.core import (Hamiltonian, TransformationType, QubitMappingType,
-                                   ChemistryOperator, MolecularGroundStateResult)
+from typing import List, Optional, Callable, Union
+
+from qiskit.aqua import QuantumInstance
+from qiskit.aqua.algorithms import MinimumEigensolver, VQE
+from qiskit.aqua.operators import Z2Symmetries
 from qiskit.chemistry import QiskitChemistryError
+from qiskit.chemistry.components.initial_states import HartreeFock
+from qiskit.chemistry.components.variational_forms import UCCSD
+from qiskit.chemistry.core import (TransformationType, QubitMappingType, ChemistryOperator,
+                                   MolecularGroundStateResult)
+from qiskit.chemistry.drivers import BaseDriver
+from qiskit.chemistry.ground_state_calculation import GroundStateCalculation
+from qiskit.providers import BaseBackend
 
 class MinimumEigensolverGroundStateCalculation(GroundStateCalculation):
     """
@@ -32,49 +40,70 @@ class MinimumEigensolverGroundStateCalculation(GroundStateCalculation):
                  orbital_reduction: Optional[List[int]] = None,
                  z2symmetry_reduction: Optional[Union[str, List[int]]] = None) -> None:
         """
-
         Args:
-            solver:
-            transformation:
-            qubit_mapping:
-            two_qubit_reduction:
-            freeze_core:
-            orbital_reduction:
-            z2symmetry_reduction:
+            solver: a minimum eigensolver
+            transformation: full or particle_hole
+            qubit_mapping: jordan_wigner, parity or bravyi_kitaev
+            two_qubit_reduction: Whether two qubit reduction should be used,
+                                  when parity mapping only
+            freeze_core: Whether to freeze core orbitals when possible
+            orbital_reduction: Orbital list to be frozen or removed
+            z2symmetry_reduction: If z2 symmetry reduction should be applied to the qubit operators
+                 that are computed. Setting 'auto' will
+                 use an automatic computation of the correct sector. If from other experiments, with
+                 the z2symmetry logic, the sector is known, then the tapering values of that sector
+                 can be provided (a list of int of values -1, and 1). The default is None
+                 meaning no symmetry reduction is done.
+                 See also :class:`~qiskit.chemistry.core.Hamiltonian` which has the core
+                 processing behind this class.
         """
 
         self._solver = solver
-        super().__init__(transformation, qubit_mapping, two_qubit_reduction, freeze_core, orbital_reduction,
-                         z2symmetry_reduction)
+        super().__init__(transformation, qubit_mapping, two_qubit_reduction, freeze_core,
+                         orbital_reduction, z2symmetry_reduction)
 
-    def compute_ground_state(self, driver, callback = None) -> GroundStateCalculationResult:
-        """
+    def compute_ground_state(self,
+                             driver: BaseDriver,
+                             callback: Optional[Callable[[List, int, str, bool, Z2Symmetries],
+                                                         MinimumEigensolver]] = None
+                             ) -> MolecularGroundStateResult:
+        """Compute Ground State properties.
 
-        Compute Ground State properties
+        Args:
+            driver: A chemistry driver.
+            callback: If not None will be called with the following values
+                num_particles, num_orbitals, qubit_mapping, two_qubit_reduction, z2_symmetries
+                in that order. This information can then be used to setup chemistry
+                specific component(s) that are needed by the chosen MinimumEigensolver.
+                The MinimumEigensolver can then be built and returned from this callback
+                for use as the solver here.
 
         Returns:
-            GroundStateCalculationResult
-
+            A molecular ground state result
+        Raises:
+            QiskitChemistryError: If no MinimumEigensolver was given and no callback is being
+                                  used that could supply one instead.
         """
 
         if self._solver is None and callback is None:
             raise QiskitChemistryError('Minimum Eigensolvaer was not provided')
-        
+
         operator, aux_operators = self._transform(driver)
-        
+
         if callback is not None:
-            num_particles = self._core.molecule_info[ChemistryOperator.INFO_NUM_PARTICLES]
-            num_orbitals = self._core.molecule_info[ChemistryOperator.INFO_NUM_ORBITALS]
+            num_particles = self.molecule_info[ChemistryOperator.INFO_NUM_PARTICLES]
+            num_orbitals = self.molecule_info[ChemistryOperator.INFO_NUM_ORBITALS]
+            z2_symmetries = self.molecule_info[ChemistryOperator.INFO_Z2SYMMETRIES]
             self._solver = callback(num_particles, num_orbitals,
-                                   self._qubit_mapping.value, self._two_qubit_reduction,
-                                   self._z2_symmetries)
-    
-        aux_operators = aux_operators if self.solver.supports_aux_operators() else None
+                                    self._qubit_mapping.value, self._two_qubit_reduction,
+                                    z2_symmetries)
+
+        aux_operators = aux_operators if self._solver.supports_aux_operators() else None
 
         raw_gs_result = self._solver.compute_minimum_eigenstate(operator, aux_operators)
 
-        return core.process_algorithm_result(raw_gs_result)
-    
+        return self._core.process_algorithm_result(raw_gs_result)
+
     @staticmethod
     def get_default_solver(quantum_instance: Union[QuantumInstance, BaseBackend]) ->\
             Optional[Callable[[List, int, str, bool, Z2Symmetries], MinimumEigensolver]]:
