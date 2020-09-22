cd0b613b0e6d251418652ed8eff5a6a33aca5e1b
Cryoris <jules.gacon@googlemail.com>
2020-09-22 14:05:36 +0200

    update (MES)GSC to new structure

--- qiskit/chemistry/ground_state_calculation/ground_state_calculation.py
+++ qiskit/chemistry/ground_state_calculation/ground_state_calculation.py
@@ -10,21 +10,13 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""
-A ground state calculation interface.
-"""
+"""The ground state calculation interface."""
 
 from abc import ABC, abstractmethod
-from typing import List, Optional, Union, Tuple, Dict, Callable
 
-from qiskit.aqua.operators import LegacyBaseOperator
-from qiskit.aqua.algorithms import MinimumEigensolver
-from qiskit.aqua.operators import Z2Symmetries
 from qiskit.chemistry.drivers import BaseDriver
-from qiskit.chemistry.core import (Hamiltonian, TransformationType, QubitMappingType,
-                                   MolecularGroundStateResult)
-from qiskit.chemistry.qubit_transformations.qubit_operator_transformation import \
-    QubitOperatorTransformation
+from qiskit.chemistry.core import MolecularGroundStateResult
+from qiskit.chemistry.qubit_transformations import QubitOperatorTransformation
 
 
 class GroundStateCalculation(ABC):
@@ -37,33 +29,33 @@ class GroundStateCalculation(ABC):
         """
         self._transformation = transformation
 
-    @abstractmethod
-    def compute_ground_state(self,
-                             driver: BaseDriver, additional_ops,
-                             callback: Optional[Callable[[List, int, str, bool, Z2Symmetries],
-                                                         MinimumEigensolver]] = None
-                             ) -> MolecularGroundStateResult:
-        """
-        Compute the ground state energy of the molecule that was supplied via the driver.
-
-        Args:
-            driver: a chemical driver
-            callback: If not None will be called with the following values
-                num_particles, num_orbitals, qubit_mapping, two_qubit_reduction, z2_symmetries
-                in that order. This information can then be used to setup chemistry
-                specific component(s) that are needed by the chosen MinimumEigensolver.
-                The MinimumEigensolver can then be built and returned from this callback
-                for use as the solver here.
+    @property
+    def transformation(self) -> QubitOperatorTransformation:
+        """Return the tranformation used obtain a qubit operator from the molecule.
 
         Returns:
-            A molecular ground state result
-        Raises:
-            QiskitChemistryError: If no MinimumEigensolver was given and no callback is being
-                                  used that could supply one instead.
+            The transformation.
         """
-        raise NotImplementedError()
+        return self._transformation
 
-    #@abstractmethod
-    #def returns_groundstate() -> bool:
-    #    # needs to return whether this calculation only returns groundstate energies or also groundstates
-    #    raise NotImplementedError
+    @abstractmethod
+    def compute_ground_state(self, driver: BaseDriver) -> MolecularGroundStateResult:
+        """Compute the ground state energy of the molecule that was supplied via the driver.
+
+        Args:
+            driver: TODO
+
+        Returns:
+            A molecular ground state result TODO
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def returns_groundstate(self) -> bool:
+        """Whether this class returns only the groundstate energy or also the groundstate itself.
+
+        Returns:
+            True, if this class also returns the ground state in the results object.
+            False otherwise.
+        """
+        raise NotImplementedError
