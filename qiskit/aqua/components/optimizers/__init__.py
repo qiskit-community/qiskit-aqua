d4e1b0d2ba6db728f2f8c5fc40d7e0d54d1f3b46
woodsp <woodsp@us.ibm.com>
2020-09-27 19:50:04 -0400

    Spelling: licences (UK variant) to licenses

--- qiskit/aqua/components/optimizers/__init__.py
+++ qiskit/aqua/components/optimizers/__init__.py
@@ -61,7 +61,7 @@ Local Optimizers
 Qiskit Aqua also provides the following optimizers, which are built-out using the optimizers from
 the `scikit-quant` package. The `scikit-quant` package is not installed by default but must be
 explicitly installed, if desired, by the user - the optimizers therein are provided under various
-licences so it has been made an optional install for the end user to choose whether to do so or
+licenses so it has been made an optional install for the end user to choose whether to do so or
 not. To install the `scikit-quant` dependent package you can use
 `pip install qiskit-aqua[skquant]`.
 
