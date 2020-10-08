7e2d46d5cc0cd942e883cef35a0507dda12d210e
Manoel Marques <manoel.marques@ibm.com>
2020-10-07 20:44:58 -0400

    fix lint

--- test/chemistry/test_driver_molecule.py
+++ test/chemistry/test_driver_molecule.py
@@ -24,10 +24,8 @@ from qiskit.chemistry.drivers import Molecule
 class TestMolecule(QiskitChemistryTestCase):
     """Test driver-independent molecule definition."""
 
-    def setUp(self):
-        super().setUp()
-
     def test_construct(self):
+        """ test construct """
         stretch = partial(
             Molecule.absolute_stretching,
             kwargs={'atom_pair': (1, 0)})
@@ -62,18 +60,21 @@ class TestMolecule(QiskitChemistryTestCase):
                 Molecule(geometry=[('H', [0., 0., 0.]), ('H', [0., 0., 1.])], masses=[1, 1, 1])
 
     def test_charge(self):
+        """ test charge """
         mol = Molecule(geometry=[('H', [0., 0., 0.]), ('H', [0., 0., 1.])])
         self.assertEqual(mol.charge, 0)
         mol.charge = 1
         self.assertEqual(mol.charge, 1)
 
     def test_multiplicity(self):
+        """ test multiplicity """
         mol = Molecule(geometry=[('H', [0., 0., 0.]), ('H', [0., 0., 1.])])
         self.assertEqual(mol.multiplicity, 1)
         mol.multiplicity = 0
         self.assertEqual(mol.multiplicity, 0)
 
     def test_stretch(self):
+        """ test stretch """
         geom = None
 
         with self.subTest('From original'):
@@ -90,6 +91,7 @@ class TestMolecule(QiskitChemistryTestCase):
             self.assertListEqual(geom[1][1], [0., 0., 3. - .1])
 
     def test_bend(self):
+        """ test bend """
         with self.subTest('pi/2 bend 1-0-2'):
             geom = Molecule.absolute_bending(atom_trio=(1, 0, 2),
                                              bend=np.pi / 2,
@@ -121,6 +123,7 @@ class TestMolecule(QiskitChemistryTestCase):
             self.assertListEqual(geom[1][1], [1., 0., 0.])
 
     def test_perturbations(self):
+        """ test perturbations """
         stretch1 = partial(Molecule.absolute_stretching, atom_pair=(1, 0))
         bend = partial(Molecule.absolute_bending, atom_trio=(1, 0, 2))
         stretch2 = partial(Molecule.absolute_stretching, atom_pair=(0, 1))
