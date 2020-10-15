1e3570ca41971f69c1941e2bb1be88dd8355a640
Max Rossmannek <oss@zurich.ibm.com>
2020-10-16 00:22:54 +0200

    Fix some docstrings and copyright years

--- qiskit/chemistry/algorithms/excited_states_solvers/excited_states_eigensolver.py
+++ qiskit/chemistry/algorithms/excited_states_solvers/excited_states_eigensolver.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2019, 2020.
+# (C) Copyright IBM 2020.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""The calculation of excited states via the qEOM algorithm"""
+"""The calculation of excited states via an Eigensolver algorithm"""
 
 import logging
 
