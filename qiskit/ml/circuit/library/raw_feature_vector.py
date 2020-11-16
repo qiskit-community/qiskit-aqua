e9475f62c00d13061c179856c44604429bdb8acb
Julien Gacon <gaconju@gmail.com>
2020-11-16 22:22:47 +0100

    attempt to fix sphinx

--- qiskit/ml/circuit/library/raw_feature_vector.py
+++ qiskit/ml/circuit/library/raw_feature_vector.py
@@ -32,31 +32,34 @@ class RawFeatureVector(BlueprintCircuit):
 
     Examples:
 
-        >>> from qiskit.ml.circuit.library import RawFeatureVector
-        >>> circuit = RawFeatureVector(4)
-        >>> circuit.num_qubits
-        2
-        >>> text_drawing = circuit.draw(output='text')
-        >>> text_drawing.encoding = 'utf-8'
-        >>> text_drawing
-             ┌──────┐
-        q_0: ┤0     ├
-             │  Raw │
-        q_1: ┤1     ├
-             └──────┘
-        >>> circuit.ordered_parameters
-        [Parameter(p[0]), Parameter(p[1]), Parameter(p[2]), Parameter(p[3])]
-        >>> import numpy as np
-        >>> state = np.array([1, 0, 0, 1]) / np.sqrt(2)
-        >>> bound = circuit.assign_parameters(state)
-        >>> text_drawing = bound.draw()
-        >>> text_drawing.encoding = 'utf-8'
-        >>> text_drawing
-             ┌──────────────────────────────────┐
-        q_0: ┤0                                 ├
-             │  initialize(0.70711,0,0,0.70711) │
-        q_1: ┤1                                 ├
-             └──────────────────────────────────┘
+    .. code-block::
+
+        from qiskit.ml.circuit.library import RawFeatureVector
+        circuit = RawFeatureVector(4)
+        print(circuit.num_qubits)
+        # prints: 2
+
+        print(circuit.draw(output='text'))
+        # prints:
+        #      ┌──────┐
+        # q_0: ┤0     ├
+        #      │  Raw │
+        # q_1: ┤1     ├
+        #      └──────┘
+
+        print(circuit.ordered_parameters)
+        # prints: [Parameter(p[0]), Parameter(p[1]), Parameter(p[2]), Parameter(p[3])]
+
+        import numpy as np
+        state = np.array([1, 0, 0, 1]) / np.sqrt(2)
+        bound = circuit.assign_parameters(state)
+        print(bound.draw())
+        # prints:
+        #      ┌──────────────────────────────────┐
+        # q_0: ┤0                                 ├
+        #      │  initialize(0.70711,0,0,0.70711) │
+        # q_1: ┤1                                 ├
+        #      └──────────────────────────────────┘
 
         """
 
