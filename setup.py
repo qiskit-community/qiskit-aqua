305a515b045d32126f24f97457441b37cfae3b0d
Matthew Treinish <mtreinish@kortar.org>
2021-01-14 08:07:58 -0500

    Add support for Python 3.9
    Python 3.9.0 was released on 10-05-2020, this commits marks the support
    of Python 3.9 in qiskit-aqua. It adds the supported python version in
    the package metadata and updates the CI configuration to run test jobs
    on Python 3.9. This follows on from Qiskit/qiskit-terra#5189,
    Qiskit/qiskit-ignis#505, and Qiskit/qiskit-aer#1071

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering"
     ),
     keywords='qiskit sdk quantum aqua',
