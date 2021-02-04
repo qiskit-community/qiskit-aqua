7c2c2e546e9002e758ff6b25f98a9a594589bd6f
Manoel Marques <Manoel.Marques@ibm.com>
2021-02-04 15:24:31 -0500

    Run mypy on python 3.7 (#1521)

--- qiskit/chemistry/transformations/fermionic_transformation.py
+++ qiskit/chemistry/transformations/fermionic_transformation.py
@@ -200,13 +200,13 @@ class FermionicTransformation(Transformation):
         # with freeze first, we have to re-base
         # the indexes for elimination according to how many orbitals were removed when freezing.
         #
-        orbitals_list = list(set(core_list + reduce_list))
+        orb_list = list(set(core_list + reduce_list))
         num_alpha = qmolecule.num_alpha
         num_beta = qmolecule.num_beta
         new_num_alpha = num_alpha
         new_num_beta = num_beta
-        if orbitals_list:
-            orbitals_list = np.array(orbitals_list)
+        if orb_list:
+            orbitals_list = np.array(orb_list)
             orbitals_list = orbitals_list[(cast(np.ndarray, orbitals_list) >= 0) &
                                           (orbitals_list < qmolecule.num_orbitals)]
 
@@ -474,7 +474,7 @@ class FermionicTransformation(Transformation):
             commutes.append(operator.commute_with(clifford))
         does_commute = np.all(commutes)
         logger.debug('  \'%s\' commutes: %s, %s', operator.name, does_commute, commutes)
-        return does_commute
+        return cast(bool, does_commute)
 
     def get_default_filter_criterion(self) -> Optional[Callable[[Union[List, np.ndarray], float,
                                                                  Optional[List[float]]], bool]]:
@@ -498,7 +498,7 @@ class FermionicTransformation(Transformation):
         return partial(filter_criterion, self)
 
     @staticmethod
-    def _pick_sector(z2_symmetries: Z2Symmetries, hf_str: np.ndarray) -> Z2Symmetries:
+    def _pick_sector(z2_symmetries: Z2Symmetries, hf_str: List[bool]) -> Z2Symmetries:
         """
         Based on Hartree-Fock bit string and found symmetries to determine the sector.
         The input z2 symmetries will be mutated with the determined tapering values.
