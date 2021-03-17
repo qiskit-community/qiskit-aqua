372f498237999779bbedd9f12635e9be243be894
a-matsuo <47442626+a-matsuo@users.noreply.github.com>
2021-03-18 07:00:30 +0900

    Bug fix for functions for stable set problems (#1555)
    * fix stable_set functions and tests
    
    * add 2021 for copyright year
    
    * fix lint

--- test/optimization/test_stable_set.py
+++ test/optimization/test_stable_set.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2020.
+# (C) Copyright IBM 2020, 2021.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -15,14 +15,11 @@
 import unittest
 from test.optimization import QiskitOptimizationTestCase
 import numpy as np
-from qiskit import BasicAer
-from qiskit.circuit.library import EfficientSU2
 
-from qiskit.aqua import aqua_globals, QuantumInstance
+from qiskit.aqua import aqua_globals
 from qiskit.optimization.applications.ising import stable_set
 from qiskit.optimization.applications.ising.common import random_graph, sample_most_likely
-from qiskit.aqua.algorithms import NumPyMinimumEigensolver, VQE
-from qiskit.aqua.components.optimizers import L_BFGS_B
+from qiskit.aqua.algorithms import NumPyMinimumEigensolver
 
 
 class TestStableSet(QiskitOptimizationTestCase):
@@ -41,26 +38,11 @@ class TestStableSet(QiskitOptimizationTestCase):
         algo = NumPyMinimumEigensolver(self.qubit_op, aux_operators=[])
         result = algo.run()
         x = sample_most_likely(result.eigenstate)
-        self.assertAlmostEqual(result.eigenvalue.real, -39.5)
-        self.assertAlmostEqual(result.eigenvalue.real + self.offset, -38.0)
+        self.assertAlmostEqual(result.eigenvalue.real, -2.5)
+        self.assertAlmostEqual(result.eigenvalue.real + self.offset, -3)
         ising_sol = stable_set.get_graph_solution(x)
-        np.testing.assert_array_equal(ising_sol, [1, 1, 0, 1, 1])
-        self.assertEqual(stable_set.stable_set_value(x, self.w), (4, False))
-
-    def test_stable_set_vqe(self):
-        """ VQE Stable set  test """
-        result = VQE(self.qubit_op,
-                     EfficientSU2(reps=3, entanglement='linear'),
-                     L_BFGS_B(maxfun=6000)).run(
-                         QuantumInstance(BasicAer.get_backend('statevector_simulator'),
-                                         seed_simulator=aqua_globals.random_seed,
-                                         seed_transpiler=aqua_globals.random_seed))
-        x = sample_most_likely(result.eigenstate)
-        self.assertAlmostEqual(result.eigenvalue, -39.5)
-        self.assertAlmostEqual(result.eigenvalue + self.offset, -38.0)
-        ising_sol = stable_set.get_graph_solution(x)
-        np.testing.assert_array_equal(ising_sol, [1, 1, 0, 1, 1])
-        self.assertEqual(stable_set.stable_set_value(x, self.w), (4.0, False))
+        np.testing.assert_array_equal(ising_sol, [1, 0, 1, 0, 1])
+        self.assertEqual(stable_set.stable_set_value(x, self.w), (3, True))
 
 
 if __name__ == '__main__':
