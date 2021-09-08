27bc883cf74386c6f6228b39c941069f32d75887
Manoel Marques <Manoel.Marques@ibm.com>
2021-09-08 13:31:34 -0400

    Handle new yfinance, fix bugs in provider (#1629)

--- qiskit/finance/data_providers/yahoo_data_provider.py
+++ qiskit/finance/data_providers/yahoo_data_provider.py
@@ -53,20 +53,20 @@ class YahooDataProvider(BaseDataProvider):
         super().__init__()
         if not _HAS_YFINANCE:
             raise MissingOptionalLibraryError(
-                libname='YFinance',
-                name='YahooDataProvider',
-                pip_install='pip install yfinance')
-        self._tickers = None  # type: Optional[Union[str, List[str]]]
+                libname="YFinance",
+                name="YahooDataProvider",
+                pip_install="pip install yfinance",
+            )
+        self._tickers = []
         tickers = tickers if tickers is not None else []
         if isinstance(tickers, list):
             self._tickers = tickers
         else:
-            self._tickers = tickers.replace('\n', ';').split(";")
+            self._tickers = tickers.replace("\n", ";").split(";")
         self._n = len(self._tickers)
 
-        self._tickers = tickers
-        self._start = start.strftime('%Y-%m-%d')
-        self._end = end.strftime('%Y-%m-%d')
+        self._start = start.strftime("%Y-%m-%d")
+        self._end = end.strftime("%Y-%m-%d")
         self._data = []
 
     def run(self) -> None:
@@ -74,25 +74,37 @@ class YahooDataProvider(BaseDataProvider):
         Loads data, thus enabling get_similarity_matrix and
         get_covariance_matrix methods in the base class.
         """
+        if len(self._tickers) == 0:
+            raise QiskitFinanceError("Missing tickers to download.")
         self._data = []
         stocks_notfound = []
         try:
-            stock_data = yf.download(' '.join(self._tickers),
-                                     start=self._start,
-                                     end=self._end,
-                                     group_by='ticker',
-                                     # threads=False,
-                                     progress=logger.isEnabledFor(logging.DEBUG))
-            for ticker_name in self._tickers:
-                stock_value = stock_data[ticker_name]['Adj Close']
+            stock_data = yf.download(
+                self._tickers,
+                start=self._start,
+                end=self._end,
+                group_by="ticker",
+                # threads=False,
+                progress=logger.isEnabledFor(logging.DEBUG),
+            )
+            if len(self._tickers) == 1:
+                ticker_name = self._tickers[0]
+                stock_value = stock_data["Adj Close"]
                 if stock_value.dropna().empty:
                     stocks_notfound.append(ticker_name)
                 self._data.append(stock_value)
+            else:
+                for ticker_name in self._tickers:
+                    stock_value = stock_data[ticker_name]["Adj Close"]
+                    if stock_value.dropna().empty:
+                        stocks_notfound.append(ticker_name)
+                    self._data.append(stock_value)
         except Exception as ex:  # pylint: disable=broad-except
-            raise QiskitFinanceError(
-                'Accessing Yahoo Data failed.') from ex
+            raise QiskitFinanceError("Accessing Yahoo Data failed.") from ex
 
         if stocks_notfound:
             raise QiskitFinanceError(
-                'No data found for this date range, symbols may be delisted: {}.'.format(
-                    stocks_notfound))
+                "No data found for this date range, symbols may be delisted: {}.".format(
+                    stocks_notfound
+                )
+            )
